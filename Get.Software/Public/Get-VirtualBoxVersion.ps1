cfbbab2e725d6be7ec9c83c56106ae996d99eca7
Trond Eirik Haavarstein <haavarstein@gmail.com>
2018-11-27 09:36:45 -0300

    Get-VirtualBoxVersiom

---
+++ Get.Software/Public/Get-VirtualBoxVersion.ps1
@@ -0,0 +1,22 @@
+Function Get-VirtualBoxVersion {
+    
+    <#
+        .NOTES
+            Author: Trond Eirik Haavarstein
+            Twitter: @xenappblog
+    #>
+    
+    
+    $url = "https://download.virtualbox.org/virtualbox/LATEST.TXT"
+
+    try {
+        $results = @()
+        $temp = New-TemporaryFile
+        Invoke-WebRequest -UseBasicParsing -Uri $url -OutFile $temp -ErrorAction SilentlyContinue
+        $Version = get-content $temp
+        Write-Output $Version
+    }
+    catch {
+        Throw "Failed to connect to URL: $url with error $_."
+    }
+}
