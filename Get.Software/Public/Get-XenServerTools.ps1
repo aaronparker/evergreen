09eeb68a3a445d4bc2dc4aa54c706b70890d0e9f
Aaron Parker <aaron@stealthpuppy.com>
2019-09-21 20:25:54 +1000

    XenServer

--- Get.Software/Public/Get-XenServerTools.ps1
+++ Get.Software/Public/Get-XenServerTools.ps1
@@ -5,16 +5,4 @@ Function Get-XenServerTools {
     $RawContent = Get-Content $TempFile
     $Table = $RawContent | ConvertFrom-Csv -Delimiter "`t" -Header "Uri", "Version", "Size", "Architecture", "Index"
     Write-Output $Table
-
-    # Write-Verbose "Setting Arguments" -Verbose
-    # $StartDTM = (Get-Date)
-    # $Vendor = "Citrix"
-    # $Product = "XenServer"
-    # $PackageName = "managementagentx64"
-    # $Version = $Latest.version
-    # $InstallerType = "msi"
-    #$Source = "$PackageName" + "." + "$InstallerType"
-    # $LogPS = "C:\Windows\Temp\$Vendor $Product $Version PS Wrapper.log"
-    # $LogApp = "C:\Windows\Temp\XS65FP1.log"
-    # $UnattendedArgs = "/i $PackageName.$InstallerType ALLUSERS=1 /Lv $LogApp /quiet /norestart"
 }
