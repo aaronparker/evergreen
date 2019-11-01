399f659adf458ef9c4544d070f2f35214a3b6545
Aaron Parker <aaron@stealthpuppy.com>
2019-11-01 20:34:34 +1100

    Get-NotepadPlusPlus: update XML error handling

--- Evergreen/Public/Get-NotepadPlusPlus.ps1
+++ Evergreen/Public/Get-NotepadPlusPlus.ps1
@@ -33,38 +33,34 @@ Function Get-NotepadPlusPlus {
     Try {
         [System.XML.XMLDocument] $xmlDocument = $Content
     }
-    Catch [System.IO.IOException] {
-        Write-Warning -Message "Failed to read XML. Check update URL: $($script:resourceStrings.Applications.NotepadPlusPlus.Uri)."
-        Throw $_.Exception.Message
-    }
     Catch [System.Exception] {
-        Write-Warning -Message "Failed to read XML."
-        Throw $_
+        Write-Warning -Message "$($MyInvocation.MyCommand): Failed to read XML. Check update URL: $($script:resourceStrings.Applications.NotepadPlusPlus.Uri)."
     }
-    
-    # Select each target XPath to return version and download details
-    If ($xmlDocument -is [System.XML.XMLDocument]) {
-        $PSObject = [PSCustomObject] @{
-            Version      = $xmlDocument.GUP.Version
-            Architecture = "x86"
-            URI          = $xmlDocument.GUP.Location
-        }
-        Write-Output -InputObject $PSObject
+    Finally {
+        # Select each target XPath to return version and download details
+        If ($xmlDocument -is [System.XML.XMLDocument]) {
+            $PSObject = [PSCustomObject] @{
+                Version      = $xmlDocument.GUP.Version
+                Architecture = "x86"
+                URI          = $xmlDocument.GUP.Location
+            }
+            Write-Output -InputObject $PSObject
 
-        # Fix the -replace with RegEx later
-        $PSObject = [PSCustomObject] @{
-            Version      = $xmlDocument.GUP.Version
-            Architecture = "x64"
-            URI          = $($xmlDocument.GUP.Location -replace "Installer.exe", "Installer.x64.exe")
+            # Fix the -replace with RegEx later
+            $PSObject = [PSCustomObject] @{
+                Version      = $xmlDocument.GUP.Version
+                Architecture = "x64"
+                URI          = $($xmlDocument.GUP.Location -replace "Installer.exe", "Installer.x64.exe")
+            }
+            Write-Output -InputObject $PSObject
         }
-        Write-Output -InputObject $PSObject
-    }
-    Else {
-        Write-Warning -Message "$($MyInvocation.MyCommand): Check update URL: $($script:resourceStrings.Applications.NotepadPlusPlus.Uri)."
-        $PSObject = [PSCustomObject] @{
-            Version      = "Unknown"
-            URI          = "Unknown"
+        Else {
+            Write-Warning -Message "$($MyInvocation.MyCommand): Check update URL: $($script:resourceStrings.Applications.NotepadPlusPlus.Uri)."
+            $PSObject = [PSCustomObject] @{
+                Version = "Unknown"
+                URI     = "Unknown"
+            }
+            Write-Output -InputObject $PSObject
         }
-        Write-Output -InputObject $PSObject
     }
 }
