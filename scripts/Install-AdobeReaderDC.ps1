8cc3a21dde19d31888cf7b713b43b9a3f8c0b3a9
Aaron Parker <aaron@stealthpuppy.com>
2019-11-08 10:30:45 +1100

    Add Install-AdobeReaderDC

---
+++ scripts/Install-AdobeReaderDC.ps1
@@ -0,0 +1,36 @@
+<#
+    .SYNOPSIS
+    Uses the Evergreen module to download and install the latest version of Adobe Reader DC
+#>
+[CmdletBinding()]
+Param()
+
+# Install the Evergreen module
+Install-Module -Name Evergreen
+
+# Target folder
+$Folder = "C:\Temp\Reader"
+New-Item -Path $Folder -ItemType Directory -Force -ErrorAction SilentlyContinue
+
+# Download Reader installer and updater
+$Reader = Get-AdobeAcrobatReaderDC | Where-Object { $_.Platform -eq "Windows" -and ($_.Language -eq "English" -or $_.Language -eq "Neutral") }
+ForEach ($File in $Reader) {
+    Invoke-WebRequest -Uri $File.Uri -OutFile (Join-Path -Path $Folder -ChildPath (Split-Path -Path $File.Uri -Leaf))
+}
+
+# Get resource strings
+$res = Export-EvergreenFunctionStrings -AppName "AdobeAcrobatReaderDC"
+
+# Install Adobe Reader
+# $exe = Get-ChildItem -Path $Folder -Filter $res.Install.Setup -Recurse
+$exe = Get-ChildItem -Path $Folder -Filter "*.exe"
+Start-Process -FilePath $exe.FullName -ArgumentList $res.Install.Virtual.Arguments -Wait
+
+# Run post install actions
+ForEach ($command in $res.Install.Virtual.PostInstall) {
+    Invoke-Command -ScriptBlock ($executioncontext.invokecommand.NewScriptBlock($command))
+}
+
+# Update Adobe Reader
+$msp = Get-ChildItem -Path $Folder -Filter "*.msp"
+Start-Process -FilePath "$env:SystemRoot\System32\msiexec.exe" -ArgumentList "/update $($msp.FullName) /quiet" -Wait
