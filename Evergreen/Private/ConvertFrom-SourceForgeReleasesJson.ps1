bd5cde0741c811506bc2cbdfd6b55a1b91831848
Aaron Parker <aaron@stealthpuppy.com>
2020-06-03 07:25:17 +1000

    Update with UTF-8 with BOM encoding for PS5

--- Evergreen/Private/ConvertFrom-SourceForgeReleasesJson.ps1
+++ Evergreen/Private/ConvertFrom-SourceForgeReleasesJson.ps1
@@ -1,4 +1,4 @@
-Function ConvertFrom-SourceForgeReleasesJson {
+﻿Function ConvertFrom-SourceForgeReleasesJson {
     <#
         .SYNOPSIS
             Validates a JSON string returned from a SourceForge releases API and returns a formatted object
@@ -33,88 +33,73 @@ Function ConvertFrom-SourceForgeReleasesJson {
         Throw [System.Management.Automation.RuntimeException] "$($MyInvocation.MyCommand): Failed to convert JSON string."
         Break
     }
-    finally {
-        # Validate that $release has the expected properties
-        Write-Verbose -Message "$($MyInvocation.MyCommand): Validating SourceForge release object."
-        $params = @{
-            ReferenceObject  = $script:resourceStrings.Properties.SourceForge
-            DifferenceObject = (Get-Member -InputObject $release -MemberType NoteProperty)
-            PassThru         = $True
-            ErrorAction      = $script:resourceStrings.Preferences.ErrorAction
-        }
-        $missingProperties = Compare-Object @params
-        If ($Null -ne $missingProperties) {
-            Write-Verbose -Message "$($MyInvocation.MyCommand): Validated successfully."
-            $validate = $True
-        }
-        Else {
-            Write-Verbose -Message "$($MyInvocation.MyCommand): Validation failed."
-            $validate = $False
-            $missingProperties | ForEach-Object {
-                Throw [System.Management.Automation.ValidationMetadataException] "$($MyInvocation.MyCommand): Property: '$_' missing"
-            }
-        }
 
-        # Find version number
+    # Validate that $release has the expected properties
+    Write-Verbose -Message "$($MyInvocation.MyCommand): Validating SourceForge release object."
+    $params = @{
+        ReferenceObject  = $script:resourceStrings.Properties.SourceForge
+        DifferenceObject = (Get-Member -InputObject $release -MemberType NoteProperty)
+        PassThru         = $True
+        ErrorAction      = $script:resourceStrings.Preferences.ErrorAction
+    }
+    $missingProperties = Compare-Object @params
+    If ($Null -ne $missingProperties) {
+        Write-Verbose -Message "$($MyInvocation.MyCommand): Validated successfully."
+        #$validate = $True
+    }
+    Else {
+        Write-Verbose -Message "$($MyInvocation.MyCommand): Validation failed."
+        #$validate = $False
+        $missingProperties | ForEach-Object {
+            Throw [System.Management.Automation.ValidationMetadataException] "$($MyInvocation.MyCommand): Property: '$_' missing"
+        }
+    }
+
+    # Find version number
+    try {
+        $Version = [RegEx]::Match($release.platform_releases.windows.filename, $MatchVersion).Captures.Groups[1].Value
+    }
+    catch {
+        Write-Verbose -Message "$($MyInvocation.MyCommand): Failed to find version number."
+        $Version = "Unknown"
+    }
+
+    # Get the downloads XML feed
+    $iwcParams = @{
+        Uri         = $Download.Feed
+        ContentType = $Download.ContentType
+        Raw         = $True
+    }
+    $Content = Invoke-WebContent @iwcParams
+
+    # Convert to an XML object
+    Try {
+        [System.XML.XMLDocument] $xmlDocument = $Content
+    }
+    Catch [System.Exception] {
+        Write-Warning -Message "$($MyInvocation.MyCommand): failed to convert feed into an XML object."
+    }
+        
+    # Select the required node/s from the XML feed
+    $nodes = Select-Xml -Xml $xmlDocument -XPath $Download.XPath | Select-Object –ExpandProperty "node"
+    $fileItems = $nodes | Select-Object -ExpandProperty $Download.FilterProperty | Where-Object { $_ -match $Version }
+
+    ForEach ($item in $fileItems) {
         try {
-            $Version = [RegEx]::Match($release.platform_releases.windows.filename, $MatchVersion).Captures.Groups[1].Value
+            $File = [RegEx]::Match($item, "$Version/$($script:resourceStrings.Filters.Filename)").Captures.Groups[1].Value
         }
         catch {
-            Write-Verbose -Message "$($MyInvocation.MyCommand): Failed to find version number."
-            $Version = "Unknown"
+            #Write-Verbose -Message "$($MyInvocation.MyCommand): not a file we want: $File."
         }
-
-        # Get the downloads XML feed
-        $iwcParams = @{
-            Uri         = $Download.Feed
-            ContentType = $Download.ContentType
-            Raw         = $True
-        }
-        $Content = Invoke-WebContent @iwcParams
-        Try {
-            [System.XML.XMLDocument] $xmlDocument = $Content
-        }
-        Catch [System.Exception] {
-            Write-Warning -Message "$($MyInvocation.MyCommand): failed to convert feed into an XML object."
-        }
-        # Select the required node/s from the XML feed
-        $nodes = Select-Xml -Xml $xmlDocument -XPath $Download.XPath | Select-Object –ExpandProperty "node"
-        $fileItems = $nodes | Select-Object -ExpandProperty $Download.FilterProperty | Where-Object { $_ -match $Version }
-
-        ForEach ($item in $fileItems) {
-            try {
-                $File = [RegEx]::Match($item, "$Version/$($script:resourceStrings.Filters.Filename)").Captures.Groups[1].Value
-            }
-            catch {
-                #Write-Verbose -Message "$($MyInvocation.MyCommand): not a file we want: $File."
-            }
-            If ($File) {
-                Write-Verbose -Message "$($MyInvocation.MyCommand): matched: $item."
-                $PSObject = [PSCustomObject] @{
-                    Version      = $Version
-                    Architecture = Get-Architecture -String $File
-                    URI          = ("$($Download.Uri)/$Version/$File" -replace " ", "%20")
-                }
-                Write-Output -InputObject $PSObject
-                Remove-Variable -Name File
-            }
-        }
-
-        # Build and array of the latest release and download URLs
-        <#If ($validate) {
-
-            # Construct the output; Return the custom object to the pipeline
-            Write-Verbose -Message "$($MyInvocation.MyCommand): Building output object."
+        If ($File) {
+            Write-Verbose -Message "$($MyInvocation.MyCommand): matched: $item."
             $PSObject = [PSCustomObject] @{
                 Version      = $Version
-                Platform     = (Get-Platform -String $release.platform_releases.windows.filename)
-                Architecture = (Get-Architecture -String $release.platform_releases.windows.filename)
-                Date         = ConvertTo-DateTime -DateTime $release.platform_releases.windows.date -Pattern $DatePattern
-                Size         = $release.platform_releases.windows.bytes
-                Md5Hash      = $release.platform_releases.windows.md5sum
-                URI          = ("$DownloadUri$($release.platform_releases.windows.filename)" -replace " ", "%20")
+                Architecture = Get-Architecture -String $File
+                URI          = "$($Download.Uri)/$Version/$File" -replace " ", "%20"
             }
             Write-Output -InputObject $PSObject
-        }#>
+            Remove-Variable -Name File
+        }
     }
 }
