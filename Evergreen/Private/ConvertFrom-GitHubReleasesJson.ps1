b1d7a50aa7939c3272a47893931eacb5ccc7fbe3
Aaron Parker <aaron@stealthpuppy.com>
2020-10-24 22:20:55 +1100

    Update OpenJDK to return more releases

--- Evergreen/Private/ConvertFrom-GitHubReleasesJson.ps1
+++ Evergreen/Private/ConvertFrom-GitHubReleasesJson.ps1
@@ -30,8 +30,10 @@ Function ConvertFrom-GitHubReleasesJson {
         Break
     }
     finally {
+        Write-Verbose -Message "$($MyInvocation.MyCommand): Found $($release.count) releases."
+
         # Ensure that we only have the latest release
-        If ($release.Count -gt 1) {
+        <#If ($release.Count -gt 1) {
             try {
                 Write-Warning -Message "$($MyInvocation.MyCommand): More than one release retrieved from GitHub."
                 $release = $release | Where-Object { $_.prerelease -eq $False } | Select-Object -First 1
@@ -39,52 +41,56 @@ Function ConvertFrom-GitHubReleasesJson {
             catch {
                 Throw [System.Management.Automation.RuntimeException] "$($MyInvocation.MyCommand): Failed to filter to latest release."
             }
-        }
+        }#>
 
         # Validate that $release has the expected properties
         Write-Verbose -Message "$($MyInvocation.MyCommand): Validating GitHub release object."
-        $params = @{
-            ReferenceObject  = $script:resourceStrings.Properties.GitHub
-            DifferenceObject = (Get-Member -InputObject $release -MemberType NoteProperty)
-            PassThru         = $True
-            ErrorAction      = $script:resourceStrings.Preferences.ErrorAction
-        }
-        $missingProperties = Compare-Object @params
-        If ($Null -ne $missingProperties) {
-            Write-Verbose -Message "$($MyInvocation.MyCommand): Validated successfully."
-            $validate = $True
-        }
-        Else {
-            Write-Verbose -Message "$($MyInvocation.MyCommand): Validation failed."
-            $validate = $False
-            $missingProperties | ForEach-Object {
-                Throw [System.Management.Automation.ValidationMetadataException] "$($MyInvocation.MyCommand): Property: '$_' missing"
+        ForEach ($item in $release) {
+            $params = @{
+                ReferenceObject  = $script:resourceStrings.Properties.GitHub
+                DifferenceObject = (Get-Member -InputObject $item -MemberType NoteProperty)
+                PassThru         = $True
+                ErrorAction      = $script:resourceStrings.Preferences.ErrorAction
+            }
+            $missingProperties = Compare-Object @params
+            If ($Null -ne $missingProperties) {
+                Write-Verbose -Message "$($MyInvocation.MyCommand): Validated successfully."
+                $validate = $True
+            }
+            Else {
+                Write-Verbose -Message "$($MyInvocation.MyCommand): Validation failed."
+                $validate = $False
+                $missingProperties | ForEach-Object {
+                    Throw [System.Management.Automation.ValidationMetadataException] "$($MyInvocation.MyCommand): Property: '$_' missing"
+                }
             }
         }
 
         # Build and array of the latest release and download URLs
         If ($validate) {
             Write-Verbose -Message "$($MyInvocation.MyCommand): Found $($release.assets.count) assets."
-            ForEach ($asset in $release.assets) {
-                If ($asset.browser_download_url -match $script:resourceStrings.Filters.WindowsInstallers) {
-                    Write-Verbose -Message "$($MyInvocation.MyCommand): Building Windows release output object."
+            ForEach ($item in $release) {
+                ForEach ($asset in $item.assets) {
+                    If ($asset.browser_download_url -match $script:resourceStrings.Filters.WindowsInstallers) {
+                        Write-Verbose -Message "$($MyInvocation.MyCommand): Building Windows release output object."
 
-                    try {
-                        $version = [RegEx]::Match($release.$VersionTag, $MatchVersion).Captures.Groups[1].Value
-                    }
-                    catch {
-                        $version = $release.$VersionTag
-                    }
+                        try {
+                            $version = [RegEx]::Match($item.$VersionTag, $MatchVersion).Captures.Groups[1].Value
+                        }
+                        catch {
+                            $version = $item.$VersionTag
+                        }
 
-                    $PSObject = [PSCustomObject] @{
-                        Version      = $version
-                        Platform     = Get-Platform -String $asset.browser_download_url
-                        Architecture = Get-Architecture -String $asset.browser_download_url
-                        Date         = ConvertTo-DateTime -DateTime $release.created_at
-                        Size         = $asset.size
-                        URI          = $asset.browser_download_url
+                        $PSObject = [PSCustomObject] @{
+                            Version      = $version
+                            Platform     = Get-Platform -String $asset.browser_download_url
+                            Architecture = Get-Architecture -String $asset.browser_download_url
+                            Date         = ConvertTo-DateTime -DateTime $item.created_at
+                            Size         = $asset.size
+                            URI          = $asset.browser_download_url
+                        }
+                        Write-Output -InputObject $PSObject
                     }
-                    Write-Output -InputObject $PSObject
                 }
             }
         }
