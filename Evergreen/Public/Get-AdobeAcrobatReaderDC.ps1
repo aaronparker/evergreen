2302a2c2acc0c052a8c4140e76e065a0e8e7d131
Aaron Parker <aaron@stealthpuppy.com>
2021-03-03 14:35:31 +1100

    Update Get-AdobeAcrobatReaderDC
    Account for changes in x64/x86 installers; Remove updater from output

--- Evergreen/Public/Get-AdobeAcrobatReaderDC.ps1
+++ Evergreen/Public/Get-AdobeAcrobatReaderDC.ps1
@@ -25,69 +25,37 @@ Function Get-AdobeAcrobatReaderDC {
     Write-Verbose -Message $res.Name
 
     #region Installer downloads
-    ForEach ($platform in $res.Get.Download.Platforms) {
-        ForEach ($language in $res.Get.Download.Languages) {
-            Write-Verbose -Message "$($MyInvocation.MyCommand): Searching download language: [$language]."
-            $Uri = $res.Get.Download.Uri -replace "#Platform", $platform.platform_type
-            $Uri = $Uri -replace "#Dist", $platform.platform_dist
-            $Uri = $Uri -replace "#Language", $language
-            $Uri = $Uri -replace "#Arch", $platform.platform_arch
-            $Uri = $Uri -replace " ", "%20"
-            $iwcParams = @{
-                Uri             = $Uri
-                Headers         = $res.Get.Download.Headers
-                UserAgent       = [Microsoft.PowerShell.Commands.PSUserAgent]::Chrome
-                UseBasicParsing = $True
-                ErrorAction     = $script:resourceStrings.Preferences.ErrorAction
-            }
-            # TODO: revert back to Invoke-WebRequestWrapper
-            #$Content = Invoke-WebRequestWrapper @iwcParams
-            $Content = Invoke-WebRequest @iwcParams
+    ForEach ($language in $res.Get.Download.Languages) {
 
-            If ($Null -ne $Content) {
-                #$ContentFromJson = $Content | ConvertFrom-Json
-                $ContentFromJson = $Content.Content | ConvertFrom-Json
-                
-                # Check properties if multiple values returned
-                If ($ContentFromJson.version.Count -eq 1) { $Version = $ContentFromJson.Version } Else { $Version = $ContentFromJson.Version | Select-Object -First 1 }
-                If ($ContentFromJson.download_url.Count -eq 1) { $downloadURI = $ContentFromJson.download_url } Else { $downloadURI = $ContentFromJson.download_url | Select-Object -First 1 }
+        Write-Verbose -Message "$($MyInvocation.MyCommand): Searching download language: [$language]."
+        $Uri = $res.Get.Download.Uri -replace "#Language", $language
+        $params = @{
+            Uri             = $Uri
+            Headers         = $res.Get.Download.Headers
+            UseBasicParsing = $True
+            ErrorAction     = $script:resourceStrings.Preferences.ErrorAction
+        }
+        try {
+            #TODO: update Invoke-RestMethodWrapper to support this query correctly
+            $Content = Invoke-RestMethod @params
+        }
+        catch {
+            Throw $_
+            Break
+        }
+
+        If ($Content) {
+            ForEach ($item in $Content) {
                 $PSObject = [PSCustomObject] @{
-                    Version  = $Version
-                    Type     = "Installer"
-                    Language = $language
-                    URI      = $downloadURI
+                    Version      = $item.Version
+                    Language     = $language
+                    Architecture = Get-Architecture -String $item.download_url
+                    Name         = $item.Name
+                    URI          = $item.download_url
                 }
                 Write-Output -InputObject $PSObject
             }
         }
-    }
-    #endregion
-
-    #region Update downloads
-    $iwcParams = @{
-        Uri         = $res.Get.Update.Uri
-        ContentType = $res.Get.Update.ContentType
-    }
-    $Content = Invoke-WebRequestWrapper @iwcParams
-
-    # Construct update download list
-    If ($Null -ne $Content) {
-        
-        ForEach ($update in $res.Get.Download.Updates.GetEnumerator()) {
-            Write-Verbose -Message "$($MyInvocation.MyCommand): Searching updates: [$($update.Name)]."
-
-            # Output objects
-            $PSObject = [PSCustomObject] @{
-                Version  = $Content
-                Type     = "Updater"
-                Language = $update.Name
-                URI      = $res.Get.Download.Updates[$update.key] -replace $res.Get.Download.ReplaceText, ($Content.Replace(".", ""))
-            }
-            Write-Output -InputObject $PSObject
-        }
-    }
-    Else {
-        Write-Warning -Message "$($MyInvocation.MyCommand): unable to retrieve content from $($res.Get.Update.Uri)."
     }
     #endregion
 }
