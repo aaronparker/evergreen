d6c25cc4eac81d309c2aa54858123e656ae673c8
Aaron Parker <aaron@stealthpuppy.com>
2021-05-10 19:00:28 +1000

    Add Wireshark

---
+++ Evergreen/Apps/Get-Wireshark.ps1
@@ -0,0 +1,46 @@
+﻿Function Get-Wireshark {
+    <#
+        .SYNOPSIS
+            Returns the available Wireshark versions and download URIs.
+
+        .NOTES
+            Author: Aaron Parker
+            Twitter: @stealthpuppy
+    #>
+    [OutputType([System.Management.Automation.PSObject])]
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSUseSingularNouns", "")]
+    [CmdletBinding(SupportsShouldProcess = $False)]
+    param (
+        [Parameter(Mandatory = $False, Position = 0)]
+        [ValidateNotNull()]
+        [System.Management.Automation.PSObject]
+        $res = (Get-FunctionResource -AppName ("$($MyInvocation.MyCommand)".Split("-"))[1]),
+
+        [Parameter(Mandatory = $False, Position = 1)]
+        [ValidateNotNull()]
+        [System.String] $Filter
+    )
+
+    # Resolve the URL to the target location
+    ForEach ($item in $res.Get.Update.Uri.GetEnumerator()) {
+        $params = @{
+            Uri       = $res.Get.Update.Uri[$item.Key]
+            UserAgent = $res.Get.Update.UserAgent
+        }
+        $Update = Invoke-RestMethodWrapper @params
+
+        If ($Null -ne $Update) {
+            ForEach ($enclosure in $Update.enclosure) {
+
+                # Build the output object; Output object to the pipeline
+                $PSObject = [PSCustomObject] @{
+                    Version      = $enclosure.version
+                    Architecture = $item.Name
+                    Size         = $enclosure.length
+                    URI          = $enclosure.url
+                }
+                Write-Output -InputObject $PSObject
+            }
+        }
+    }
+}
