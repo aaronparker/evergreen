1ed56e9c151b95f4be2915a0164d93236f4a6bd0
Aaron Parker <aaron@stealthpuppy.com>
2021-05-16 22:33:00 +1000

    Add VeraCrypt #160; Update Get-SourceForgeRepoRelease

---
+++ Evergreen/Apps/Get-VeraCrypt.ps1
@@ -0,0 +1,33 @@
+Function Get-VeraCrypt {
+    <#
+        .SYNOPSIS
+            Get the current version and download URL for VeraCrypt.
+
+        .NOTES
+            Site: https://stealthpuppy.com
+            Author: Aaron Parker
+            Twitter: @stealthpuppy
+    #>
+    [OutputType([System.Management.Automation.PSObject])]
+    [CmdletBinding(SupportsShouldProcess = $False)]
+    param (
+        [Parameter(Mandatory = $False, Position = 0)]
+        [ValidateNotNull()]
+        [System.Management.Automation.PSObject]
+        $res = (Get-FunctionResource -AppName ("$($MyInvocation.MyCommand)".Split("-"))[1]),
+
+        [Parameter(Mandatory = $False, Position = 1)]
+        [ValidateNotNull()]
+        [System.String] $Filter
+    )
+
+    # Get latest version and download latest release via SourceForge API
+    # Convert the returned release data into a useable object with Version, URI etc.
+    $params = @{
+        Uri          = $res.Get.Update.Uri
+        Download     = $res.Get.Download
+        MatchVersion = $res.Get.MatchVersion
+    }
+    $object = Get-SourceForgeRepoRelease @params
+    Write-Output -InputObject $object
+}
