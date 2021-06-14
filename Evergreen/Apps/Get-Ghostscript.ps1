2e9b8daf52c74f978e8ef2f1759a84e02630fcd5
Andrew Cooper <a.coop@outlook.com>
2021-06-14 10:58:39 +0100

    Add Coretto, FreeFem, Ghostscript, TeXstudio, Scratch and 4 more

---
+++ Evergreen/Apps/Get-Ghostscript.ps1
@@ -0,0 +1,33 @@
+Function Get-Ghostscript {
+    <#
+        .SYNOPSIS
+            Returns the available Ghostscript versions.
+
+        .NOTES
+            Author: Andrew Cooper 
+            Twitter: @adotcoop
+    #>
+    [OutputType([System.Management.Automation.PSObject])]
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSUseSingularNouns", "")]
+    [CmdletBinding(SupportsShouldProcess = $False)]
+    param (
+        [Parameter(Mandatory = $False, Position = 0)]
+        [ValidateNotNull()]
+        [System.Management.Automation.PSObject]
+        $res = (Get-FunctionResource -AppName ("$($MyInvocation.MyCommand)".Split("-"))[1]),
+
+        [Parameter(Mandatory = $False, Position = 1)]
+        [ValidateNotNull()]
+        [System.String] $Filter
+    )
+
+    # Pass the repo releases API URL and return a formatted object
+    $params = @{
+        Uri          = $res.Get.Uri
+        MatchVersion = $res.Get.MatchVersion
+        Filter       = $res.Get.MatchFileTypes
+	VersionTag   = $res.Get.VersionTag
+    }
+    $object = Get-GitHubRepoRelease @params
+    Write-Output -InputObject $object
+}
