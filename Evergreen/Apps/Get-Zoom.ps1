1a836f43caf3903da10bc0b4e09430ed6f461c77
Aaron Parker <aaron@stealthpuppy.com>
2021-07-05 17:38:14 +1000

    Capture null URI

--- Evergreen/Apps/Get-Zoom.ps1
+++ Evergreen/Apps/Get-Zoom.ps1
@@ -27,11 +27,16 @@ Function Get-Zoom {
             $redirectUrl = Resolve-SystemNetWebRequest -Uri $res.Get.Download[$platform][$installer]
 
             # Match the URL without the text after the ?
-            try {
-                $Url = [RegEx]::Match($redirectUrl.ResponseUri.AbsoluteUri, $res.Get.MatchUrl).Captures.Groups[1].Value
+            If ($Null -eq $redirectUrl) {
+                $Url = $script:resourceStrings.Uri.Issues
             }
-            catch {
-                $Url = $redirectUrl.ResponseUri.AbsoluteUri
+            Else {
+                try {
+                    $Url = [RegEx]::Match($redirectUrl.ResponseUri.AbsoluteUri, $res.Get.MatchUrl).Captures.Groups[1].Value
+                }
+                catch {
+                    $Url = $redirectUrl.ResponseUri.AbsoluteUri
+                }
             }
 
             # Match version number from the download URL
