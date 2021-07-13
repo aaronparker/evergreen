17a9d2402ee7ea19713e4f27fb4271bbe955be2f
Aaron Parker <aaron@stealthpuppy.com>
2021-07-13 17:30:41 +1000

    Update GitHub API limit output

--- ci/Tests.ps1
+++ ci/Tests.ps1
@@ -60,25 +60,3 @@ Else {
 
 # Line break for readability in AppVeyor console
 Write-Host ""
-
-
-# Output GitHub rate limit
-try {
-    $SslProtocol = "Tls12"
-    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::$SslProtocol
-    $params = @{
-        ContentType        = "application/vnd.github.v3+json"
-        ErrorAction        = "SilentlyContinue"
-        MaximumRedirection = 0
-        DisableKeepAlive   = $true
-        UseBasicParsing    = $true
-        UserAgent          = [Microsoft.PowerShell.Commands.PSUserAgent]::Chrome
-        Uri                = "https://api.github.com/rate_limit"
-    }
-    $GitHubRate = Invoke-RestMethod @params
-}
-catch {
-}
-Write-Host "We have $($GitHubRate.rate.remaining) requests left to the GitHub API in this window."
-$ResetWindow = [System.TimeZone]::CurrentTimeZone.ToLocalTime(([System.DateTime]'1/1/1970').AddSeconds($GitHubRate.rate.reset))
-Write-Host "GitHub rate limit window resets at: $($ResetWindow.ToShortDateString()) $($ResetWindow.ToShortTimeString())."
