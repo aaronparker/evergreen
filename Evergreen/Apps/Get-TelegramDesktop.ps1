50e56d4eae102ff607cd612fff7d53a97ec84555
Aaron Parker <aaron@stealthpuppy.com>
2021-08-16 23:28:59 +1000

    Update TelegramDesktop
    Telegram adds releases to GitHub that don't match the current Windows release

--- Evergreen/Apps/Get-TelegramDesktop.ps1
+++ Evergreen/Apps/Get-TelegramDesktop.ps1
@@ -22,10 +22,19 @@ Function Get-TelegramDesktop {
 
     # Pass the repo releases API URL and return a formatted object
     $params = @{
-        Uri          = $res.Get.Uri
-        MatchVersion = $res.Get.MatchVersion
-        Filter       = $res.Get.MatchFileTypes
+        Uri               = $res.Get.Update.Uri
+        MatchVersion      = $res.Get.Update.MatchVersion
+        Filter            = $res.Get.Update.MatchFileTypes
+        ReturnVersionOnly = $True
     }
     $object = Get-GitHubRepoRelease @params
-    Write-Output -InputObject $object
+
+    # Build the output object
+    If ($Null -ne $object) {
+        $PSObject = [PSCustomObject] @{
+            Version = $object.Version
+            URI     = $res.Get.Download.Uri -replace $res.Get.Download.ReplaceText, $object.Version
+        }
+        Write-Output -InputObject $PSObject
+    }
 }
