f6cc64e4de7f5735f5c4ff6767bacbd341a05512
Aaron Parker <aaron@stealthpuppy.com>
2021-08-19 23:08:49 +1000

    #223
    Ben Olden-Cooligan NAPS2

---
+++ Evergreen/Apps/Get-Naps2.ps1
@@ -0,0 +1,32 @@
+Function Get-Naps2 {
+    <#
+        .SYNOPSIS
+            Returns the latest Ben Olden-Cooligan NAPS2 version number and download.
+
+        .NOTES
+            Author: Aaron Parker
+            Twitter: @stealthpuppy
+    #>
+    [OutputType([System.Management.Automation.PSObject])]
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSUseSingularNouns", "")]
+    [CmdletBinding(SupportsShouldProcess = $False)]
+    param (
+        [Parameter(Mandatory = $False, Position = 0)]
+        [ValidateNotNull()]
+        [System.Management.Automation.PSObject]
+        $res = (Get-FunctionResource -AppName ("$($MyInvocation.MyCommand)".Split("-"))[1]),
+
+        [Parameter(Mandatory = $False, Position = 1)]
+        [ValidateNotNull()]
+        [System.String] $Filter
+    )
+
+    # Pass the repo releases API URL and return a formatted object
+    $params = @{
+        Uri          = $res.Get.Uri
+        MatchVersion = $res.Get.MatchVersion
+        Filter       = $res.Get.MatchFileTypes
+    }
+    $object = Get-GitHubRepoRelease @params
+    Write-Output -InputObject $object
+}
