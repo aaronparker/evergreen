8e783850cc546e10aed3ee88c7f5b75ff162df12
Jonathan Pitre <jakeld@gmail.com>
2021-10-20 14:17:26 -0400

    Add VisualCppRedistAIO

---
+++ Evergreen/Apps/Get-VisualCppRedistAIO.ps1
@@ -0,0 +1,32 @@
+Function Get-VisualCppRedistAIO {
+    <#
+        .SYNOPSIS
+            Returns the latest VisualCppRedistAIO version number and download.
+
+        .NOTES
+            Author: Dan Gough
+            Twitter: @packageologist
+    #>
+    [OutputType([System.Management.Automation.PSObject])]
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSUseSingularNouns", "")]
+    [CmdletBinding(SupportsShouldProcess = $False)]
+    param (
+        [Parameter(Mandatory = $False, Position = 0)]
+        [ValidateNotNull()]
+        [System.Management.Automation.PSObject]
+        $res = (Get-FunctionResource -AppName ("$($MyInvocation.MyCommand)".Split("-"))[1]),
+
+        [Parameter(Mandatory = $False, Position = 1)]
+        [ValidateNotNull()]
+        [System.String] $Filter
+    )
+
+    # Pass the repo releases API URL and return a formatted object
+    $params = @{
+        Uri          = $res.Get.Uri
+        MatchVersion = $res.Get.MatchVersion
+        Filter       = $res.Get.MatchFileTypes
+    }
+    $object = Get-GitHubRepoRelease @params
+    Write-Output -InputObject $object
+}
