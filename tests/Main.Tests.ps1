73b052a95bbed283a53d1ce92f0642fdd66052d6
Aaron Parker <aaron@stealthpuppy.com>
2022-05-16 08:56:14 +1000

    Update workflows; Update tests
    Remove redundant workflows; Remove PSScriptAnalyzer from tests

--- tests/Main.Tests.ps1
+++ tests/Main.Tests.ps1
@@ -12,9 +12,6 @@ BeforeDiscovery {
     $scripts = Get-ChildItem -Path $moduleParent -Recurse -Include *.ps1, *.psm1
     $testCase = $scripts | ForEach-Object { @{file = $_ } }
 
-    # Get the ScriptAnalyzer rules
-    $scriptAnalyzerRules = Get-ScriptAnalyzerRule
-
     # Find module scripts to create the test cases
     $scripts = Get-ChildItem -Path $moduleParent -Recurse -Include *.ps1
     $testCase = $scripts | ForEach-Object { @{file = $_ } }
@@ -30,20 +27,6 @@ Describe "General project validation" {
         $errors = $null
         $null = [System.Management.Automation.PSParser]::Tokenize($contents, [ref]$errors)
         $errors.Count | Should -Be 0
-    }
-
-    It "Script <file.Name> should pass ScriptAnalyzer" -TestCases $testCase {
-        param ($file)
-        $analysis = Invoke-ScriptAnalyzer -Path $file.FullName -ExcludeRule @('PSAvoidGlobalVars', 'PSAvoidUsingWMICmdlet') -Severity @('Warning', 'Error')
-
-        ForEach ($rule in $scriptAnalyzerRules) {
-            If ($analysis.RuleName -contains $rule) {
-                $analysis |
-                Where-Object RuleName -EQ $rule -OutVariable failures |
-                Out-Default
-                $failures.Count | Should -Be 0
-            }
-        }
     }
 }
 
