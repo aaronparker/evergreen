b7c96a1c0ed35bdc2001f2a0b8f977f8d39ff71f
Aaron Parker <aaron@stealthpuppy.com>
2022-06-02 21:42:08 +1000

    Minor updates to `VMwareTools`

--- Evergreen/Apps/Get-VMwareTools.ps1
+++ Evergreen/Apps/Get-VMwareTools.ps1
@@ -30,15 +30,23 @@
     Else {
         # Format the results returns and convert into an array that we can sort and use
         Write-Verbose -Message "$($MyInvocation.MyCommand): Filtering version table with $($Content.Count) lines."
-        $Lines = $Content | Where-Object { $_ –notmatch "^#" }
-        $Lines = $Lines | Where-Object { $_ –notmatch "esx/0.0" }
+        $Lines = $Content | Where-Object { $_ -notmatch "^#" }
+        $Lines = $Lines | Where-Object { $_ -notmatch "esx/0.0" }
         Write-Verbose -Message "$($MyInvocation.MyCommand): Filtered to $($Lines.Count) lines."
+
         Write-Verbose -Message "$($MyInvocation.MyCommand): Selecting first 20 lines."
         $Lines = $Lines | Select-Object -First 20
+
         Write-Verbose -Message "$($MyInvocation.MyCommand): Convert to table."
-        $Lines = $Lines | ForEach-Object { $_ -replace '\s+', ',' }
-        $VersionTable = $Lines | ConvertFrom-Csv -Delimiter "," -Header $res.Get.Update.CsvHeaders | `
-            Sort-Object -Property @{ Expression = { [System.Version]$_.Version }; Descending = $true }
+        try {
+            $Lines = $Lines | ForEach-Object { $_ -replace '\s+', ',' }
+            $VersionTable = $Lines | ConvertFrom-Csv -Delimiter "," -Header $res.Get.Update.CsvHeaders | `
+                Sort-Object -Property @{ Expression = { [System.Version]$_.Version }; Descending = $true }
+        }
+        catch {
+            Write-Error -Message "$($MyInvocation.MyCommand): Failed to convert version source to a table."
+        }
+
         $LatestVersion = $VersionTable | Select-Object -First 1
         Write-Verbose -Message "$($MyInvocation.MyCommand): Found version: $($LatestVersion.Version)-$($LatestVersion.ClientBuild)"
 
@@ -52,7 +60,7 @@
                 URI          = $res.Get.Download.Uri -replace "#architecture", $architecture.Key `
                     -replace "#version", $LatestVersion.Version `
                     -replace "#build", $LatestVersion.ClientBuild `
-                    -replace "#type", $res.Get.Download.Architecture[$architecture.Key]
+                    -replace "#architecture", $res.Get.Download.Architecture[$architecture.Key]
             }
             Write-Output -InputObject $PSObject
         }
