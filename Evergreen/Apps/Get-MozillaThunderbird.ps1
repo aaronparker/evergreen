c14a63202fa932b04e845d75dc245e7c79697668
Aaron Parker <aaron@stealthpuppy.com>
2022-06-11 11:05:26 +1000

    Align Thunderbird to Firefox

--- Evergreen/Apps/Get-MozillaThunderbird.ps1
+++ Evergreen/Apps/Get-MozillaThunderbird.ps1
@@ -14,21 +14,25 @@ Function Get-MozillaThunderbird {
         [Parameter(Mandatory = $False, Position = 0)]
         [ValidateNotNull()]
         [System.Management.Automation.PSObject]
-        $res = (Get-FunctionResource -AppName ("$($MyInvocation.MyCommand)".Split("-"))[1])
+        $res = (Get-FunctionResource -AppName ("$($MyInvocation.MyCommand)".Split("-"))[1]),
+
+        [Parameter(Mandatory = $False, Position = 1)]
+        [ValidateNotNull()]
+        [System.String[]] $Language = @("en-US")
     )
 
     # Get latest Thunderbird version
     $thunderbirdVersions = Invoke-RestMethodWrapper -Uri $res.Get.Update.Uri
 
     # Construct custom object with output details
-    ForEach ($language in $res.Get.Download.Languages) {
+    ForEach ($lang in $Language) {
         ForEach ($channel in $res.Get.Update.Channels) {
             ForEach ($platform in $res.Get.Download.Platforms) {
 
                 # Select the download file for the selected platform
                 ForEach ($installer in $res.Get.Download.Uri[$channel].GetEnumerator()) {
                     $params = @{
-                        Uri = (($res.Get.Download.Uri[$channel][$installer.Key] -replace $res.Get.Download.Text.Platform, $platform) -replace $res.Get.Download.Text.Language, $language)
+                        Uri = (($res.Get.Download.Uri[$channel][$installer.Key] -replace $res.Get.Download.Text.Platform, $platform) -replace $res.Get.Download.Text.Language, $lang)
                     }
                     $response = Resolve-SystemNetWebRequest @params
 
@@ -37,7 +41,7 @@ Function Get-MozillaThunderbird {
                         Version      = $thunderbirdVersions.$channel
                         Architecture = Get-Architecture -String $platform
                         Channel      = $channel
-                        Language     = $language
+                        Language     = $lang
                         Type         = [System.IO.Path]::GetExtension($response.ResponseUri.AbsoluteUri).Split(".")[-1]
                         Filename     = (Split-Path -Path $response.ResponseUri.AbsoluteUri -Leaf).Replace('%20', ' ')
                         URI          = $response.ResponseUri.AbsoluteUri
