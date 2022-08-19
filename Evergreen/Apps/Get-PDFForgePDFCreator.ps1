8e52255e005857ef1078088b2b10e08201415700
Aaron Parker <aaron@stealthpuppy.com>
2022-08-20 01:26:34 +1000

    Fix #368
    Change approach to finding updates for PDFForgePDFCreator

--- Evergreen/Apps/Get-PDFForgePDFCreator.ps1
+++ Evergreen/Apps/Get-PDFForgePDFCreator.ps1
@@ -17,13 +17,27 @@ Function Get-PDFForgePDFCreator {
         $res = (Get-FunctionResource -AppName ("$($MyInvocation.MyCommand)".Split("-"))[1])
     )
 
-    # Get latest version and download latest release via SourceForge API
-    # Convert the returned release data into a useable object with Version, URI etc.
+    # Query the update API
     $params = @{
-        Uri          = $res.Get.Update.Uri
-        Download     = $res.Get.Download
-        MatchVersion = $res.Get.MatchVersion
+        Uri         = $res.Get.Update.Uri
+        ContentType = $res.Get.Update.ContentType
     }
-    $object = Get-SourceForgeRepoRelease @params
-    Write-Output -InputObject $object
+    $Update = Invoke-RestMethodWrapper @params
+    if ($Null -ne $Update) {
+
+        # Select the latest version
+        $LatestUpdate = $Update | Where-Object { $_.isStable -eq $True } | `
+            Sort-Object -Property @{ Expression = { [System.Version]$_.Version }; Descending = $true } | `
+            Select-Object -First 1
+
+        $PSObject = [PSCustomObject] @{
+            Version  = $LatestUpdate.version
+            Date     = ConvertTo-DateTime -DateTime $LatestUpdate.releaseDate -Pattern "yyyy-MM-dd"
+            Size     = $LatestUpdate.downloads[0].size
+            MD5      = $LatestUpdate.downloads[0].md5
+            Filename = $LatestUpdate.downloads[0].filename
+            URI      = $LatestUpdate.downloads[0].sourceUrl
+        }
+        Write-Output -InputObject $PSObject
+    }
 }
