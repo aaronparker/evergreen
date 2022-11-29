dc746f919e2ce439c80f44d96c7cab07caa14b96
Kirill Trofimov <work@g3rhard.cc>
2022-11-29 11:45:53 +0100

    Update DockerDesktop with a few releases in release URL and switch from MSI to exe

--- Evergreen/Apps/Get-DockerDesktop.ps1
+++ Evergreen/Apps/Get-DockerDesktop.ps1
@@ -16,15 +16,19 @@ Function Get-DockerDesktop {
         $res = (Get-FunctionResource -AppName ("$($MyInvocation.MyCommand)".Split("-"))[1])
     )
 
-    $Update = Invoke-RestMethodWrapper -Uri $res.Get.Update.Uri
-    if ($Null -ne $Update) {
-        $PSObject = [PSCustomObject] @{
-            Version = $Update.enclosure.shortVersionString
-            Build   = $Update.enclosure.version
-            Size    = $Update.enclosure.length
-            Type    = Get-FileType -File $($Update.enclosure.url | Where-Object { $_ -match "\.msi$" } | Select-Object -First 1)
-            URI     = $Update.enclosure.url | Where-Object { $_ -match "\.msi$" } | Select-Object -First 1
+    $Updates = Invoke-RestMethodWrapper -Uri $res.Get.Update.Uri
+
+    foreach ($Update in $Updates) {
+        if ($Null -ne $Update) {
+            $PSObject = [PSCustomObject] @{
+                Version = $Update.enclosure.shortVersionString
+                Build   = $Update.enclosure.version
+                Size    = $Update.enclosure.length
+                Type    = Get-FileType -File $($Update.enclosure.url | Where-Object { $_ -match "\.exe$" } | Select-Object -First 1)
+                URI     = $Update.enclosure.url | Where-Object { $_ -match "\.exe$" } | Select-Object -First 1
+            }
+            Write-Output -InputObject $PSObject
         }
-        Write-Output -InputObject $PSObject
     }
+
 }
