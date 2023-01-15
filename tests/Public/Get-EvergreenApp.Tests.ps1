be4ea6d37ee51238cd361dbb837d847ec497e798
Aaron Parker <aaron@stealthpuppy.com>
2023-01-15 12:52:32 +1100

    Update Get-EvergreenApp.Tests.ps1
    Update for loops

--- tests/Public/Get-EvergreenApp.Tests.ps1
+++ tests/Public/Get-EvergreenApp.Tests.ps1
@@ -18,41 +18,45 @@ BeforeDiscovery {
 }
 
 Describe -Tag "Get" -Name "Get-EvergreenApp works with supported application: <application>" -ForEach $Applications {
-    Context "Validate Get-EvergreenApp works with: <application>." {
-        BeforeAll {
-            # Renaming the automatic $_ variable to $application to make it easier to work with
-            $application = $_
-            $Output = Get-EvergreenApp -Name $application -WarningAction "SilentlyContinue"
+    BeforeAll {
+        # Renaming the automatic $_ variable to $application to make it easier to work with
+        $application = $_
+        $Output = Get-EvergreenApp -Name $application -WarningAction "SilentlyContinue"
 
-            # RegEx
-            $MatchUrl = "(\s*\[+?\s*(\!?)\s*([a-z]*)\s*\|?\s*([a-z0-9\.\-_]*)\s*\]+?)?\s*([^\s]+)\s*"
-            $MatchVersions = "(\d+(\.\d+){1,4}).*|(\d+)|^[0-9]{4}$|insider|Latest|Unknown|Preview|Any|jdk*|RateLimited"
+        # RegEx
+        $MatchUrl = "(\s*\[+?\s*(\!?)\s*([a-z]*)\s*\|?\s*([a-z0-9\.\-_]*)\s*\]+?)?\s*([^\s]+)\s*"
+        $MatchVersions = "(\d+(\.\d+){1,4}).*|(\d+)|^[0-9]{4}$|insider|Latest|Unknown|Preview|Any|jdk*|RateLimited"
+    }
+
+    Context "Validate Get-EvergreenApp works with: <application>." -ForEach $Output {
+        BeforeAll {
+            $Item = $_
         }
 
-        It "Output from <application>: should not be null" -ForEach $Output {
+        It "Output from <application>: should not be null" -ForEach $Item {
             $_ | Should -Not -BeNullOrEmpty
         }
 
-        It "Output from <application>: should return the expected output type" -ForEach $Output {
+        It "Output from <application>: should return the expected output type" -ForEach $Item {
             $_ | Should -BeOfType "PSCustomObject"
         }
 
-        It "Get-EvergreenApp -Name <application> should return a count of 1 or more" -ForEach $Output {
+        It "Get-EvergreenApp -Name <application> should return a count of 1 or more" -ForEach $Item {
             ($_ | Measure-Object).Count | Should -BeGreaterThan 0
         }
 
         # Test that the output has a Version property and that property is a string
-        It "Output for <application> should have a Version property that is a string" -ForEach $Output {
+        It "Output for <application> should have a Version property that is a string" -ForEach $Item {
             $_.Version | Should -BeOfType [System.String]
         }
 
         # Test that output with Version property is valid
-        It "Output for <application> should have a valid version number" -ForEach $Output {
+        It "Output for <application> should have a valid version number" -ForEach $Item {
             $_.Version | Should -Match $MatchVersions
         }
 
         # Test that the output has a URI property and that property is a string
-        It "Output for <application> should have a URI property that is a string" -ForEach $Output {
+        It "Output for <application> should have a URI property that is a string" -ForEach $Item {
             $_.URI | Should -BeOfType [System.String]
         }
     }
