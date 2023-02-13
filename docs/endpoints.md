ef8ada2cc4d0a39a1ce8bb1b605308e426f992ef
Aaron Parker <aaron@stealthpuppy.com>
2023-02-13 17:17:09 +1100

    Change endpoints to versions

--- docs/endpoints.md
+++ docs/endpoints.md
@@ -8,17 +8,17 @@ The list of endpoints can be imported into firewall or proxy server systems wher
 
 The API provides two lists of endpoints - URLs used by Evergreen to determine application versions and downloads, and URLs used to download application installers.
 
-### Return a list of URLs used to determine updates
+### Return a list of URLs used to determine application versions
 
-The list of URLs used to determine updates is used by the Evergreen PowerShell module. This information is already hosted in the Evergreen API and is not required if you are using the `/app/{appName}` API endpoint to find application version and installers. This list of URLs will update as supported applications in Evergreen are updated.
+The list of URLs used to determine application versions is used by the Evergreen PowerShell module. This information is already hosted in the Evergreen API and is not required if you are using the `/app/{appName}` API endpoint to find application version and installers. This list of URLs will update as supported applications in Evergreen are updated.
 
 ```powershell
-PS C:\> Invoke-RestMethod -Uri "https://evergreen-api.stealthpuppy.com/endpoints/updates"
+PS C:\> Invoke-RestMethod -Uri "https://evergreen-api.stealthpuppy.com/endpoints/versions"
 ```
 
 ### Return a list of URLs used to download application installers
 
-This list of URLs is used when downloading application installers as determined by Evergreen. This list of URLs should remain largely static; however the list can change for some applications. For example, `Get-EvergrenApp -Name "VideoLanVlcPlayer` will return a list of download URLs based on the list of mirrors returned by VLC.
+This list of URLs is used when downloading application installers as determined by Evergreen. This list of URLs should remain largely static; however the list can change for some applications. For example, `Get-EvergreenApp -Name "VideoLanVlcPlayer` will return a list of download URLs based on the list of mirrors returned by VLC.
 
 ```powershell
 PS C:\> Invoke-RestMethod -Uri "https://evergreen-api.stealthpuppy.com/endpoints/downloads"
--- scripts/Get-Endpoints.ps1
+++ scripts/Get-Endpoints.ps1
@@ -1,27 +1,29 @@
 # Read the application manifests and output URLs as a PSObject
-$UrlMatch = "(http|https):\/\/(.*[a-zA-Z0-9_\-]+\.[a-zA-Z0-9_\-]+)/"
-$EndpointsUpdates = Get-ChildItem -Path "/Users/aaron/projects/evergreen/Evergreen/Manifests" -Recurse -Include "*.json" | ForEach-Object {
-    [PSCustomObject]@{
-        Application = $_.BaseName
-        Endpoints   = ((Select-String -Path $_.FullName -Pattern $UrlMatch).Matches.Value | Select-Object -Unique | Sort-Object)
-    }
-}
+# $UrlMatch = "(http|https):\/\/(.*[a-zA-Z0-9_\-]+\.[a-zA-Z0-9_\-]+)/"
+# $UrlMatch = "(?:(?:http|https):\/\/)(.*[a-zA-Z0-9_\-]+\.[a-zA-Z0-9_\-]+)"
+# Select-String -Path "/Users/aaron/projects/evergreen/Evergreen/Manifests/MicrosoftEdge.json" -Pattern $UrlMatch
 
 $Namespace = "037069e7da3e4944be2cbc97c92409a5"
-wrangler kv:key put "endpoints-updates" $($EndpointsUpdates | ConvertTo-Json) --namespace-id=$Namespace
+$UrlMatch = "http[s]?\:\/\/([^\/?#]+)(?:[\/?#]|$)"
 
-$EndpointsDownloads = Get-ChildItem -Path "/Users/aaron/projects/apptracker/json" -Recurse -Include "*.json" | ForEach-Object {
+# Get endpoint URLs from Evergreen manifests and post to the version endpoint
+$Endpoints = Get-ChildItem -Path "/Users/aaron/projects/evergreen/Evergreen/Manifests" -Recurse -Include "*.json" | ForEach-Object {
     [PSCustomObject]@{
         Application = $_.BaseName
-        Endpoints   = ((Select-String -Path $_.FullName -Pattern $UrlMatch).Matches.Value | Select-Object -Unique | Sort-Object)
+        Endpoints   = ((((Select-String -Path $_.FullName -Pattern $UrlMatch).Matches.Value | Select-Object -Unique | Sort-Object) -replace "http://|https://", "").TrimEnd("/|#"))
     }
 }
+$Endpoints | ConvertTo-Json | Out-File -FilePath "./Endpoints.json" -Encoding "Utf8" -NoNewline
+wrangler kv:key put "endpoints-versions" --path="./Endpoints.json" --namespace-id=$Namespace
+Remove-Item -Path "./Endpoints.json"
 
-wrangler kv:key put "endpoints-downloads" $($EndpointsDownloads | ConvertTo-Json) --namespace-id=$Namespace
-
-
-
-
-
-$UrlMatch = "(?:(?:http|https):\/\/)(.*[a-zA-Z0-9_\-]+\.[a-zA-Z0-9_\-]+)"
-Select-String -Path "/Users/aaron/projects/evergreen/Evergreen/Manifests/MicrosoftEdge.json" -Pattern $UrlMatch
+# Get endpoint URLs for downloads from the Evergreen AppTracker and port to the downloads endpoint
+$Endpoints = Get-ChildItem -Path "/Users/aaron/projects/apptracker/json" -Recurse -Include "*.json" | ForEach-Object {
+    [PSCustomObject]@{
+        Application = $_.BaseName
+        Endpoints   = ((((Select-String -Path $_.FullName -Pattern $UrlMatch).Matches.Value | Select-Object -Unique | Sort-Object) -replace "http://|https://", "").TrimEnd("/|#"))
+    }
+}
+$Endpoints | ConvertTo-Json | Out-File -FilePath "./Endpoints.json" -Encoding "Utf8" -NoNewline
+wrangler kv:key put "endpoints-downloads" --path="./Endpoints.json" --namespace-id=$Namespace
+Remove-Item -Path "./Endpoints.json"
