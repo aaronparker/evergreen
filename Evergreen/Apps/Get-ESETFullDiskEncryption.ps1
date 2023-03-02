da314055bec2eddb6abd12f0e6decf5ed1ad9d1d
Aaron Parker <aaron@stealthpuppy.com>
2023-03-02 12:03:29 +1100

    #466
    Update code to step through each language and return the latest version for that language to account for instances where a specific language has a more recent version than other languages

--- Evergreen/Apps/Get-ESETFullDiskEncryption.ps1
+++ Evergreen/Apps/Get-ESETFullDiskEncryption.ps1
@@ -22,35 +22,50 @@ function Get-ESETFullDiskEncryption {
     $Metadata = Invoke-RestMethodWrapper @params
     if ($null -ne $Metadata) {
 
-        try {
-            Write-Verbose -Message "$($MyInvocation.MyCommand): Filter for metadata and sorting updates."
-            $Latest = ('{' + $(([Regex]::Matches($Metadata, '(?<={)(.*?)(?=}],"switch")')).Value) + '}]}' | ConvertFrom-Json).info | `
+        # Grab the JSON metadata from the returned object
+        Write-Verbose -Message "$($MyInvocation.MyCommand): Filter for metadata and sorting updates."
+        $InfoData = ('{' + $(([Regex]::Matches($Metadata, '(?<={)(.*?)(?=}],"switch")')).Value) + '}]}' | ConvertFrom-Json).info
+
+        # Find all the supported languages because the versions can be different per language
+        $Languages = $InfoData.Language | Select-Object -Unique
+        Write-Verbose -Message "$($MyInvocation.MyCommand): Found $($Language.Count) languages."
+
+        # Step through each of the supported languages
+        foreach ($Language in $Languages) {
+
+            # Find the latest version number of this language
+            Write-Verbose -Message "$($MyInvocation.MyCommand): Filter on language: $Language."
+            $Latest = $InfoData | Where-Object { $_.Language -eq $Language } | `
                 Sort-Object -Property @{ Expression = { [System.Version]$_.Version }; Descending = $true } | `
-                Where-Object { $_.OSType -eq "windows" } | Select-Object -First 1
+                Where-Object { $_.OSType -eq "windows" } | `
+                Select-Object -First 1
+
             Write-Verbose -Message "$($MyInvocation.MyCommand): Found product: $($Latest.Description)."
             Write-Verbose -Message "$($MyInvocation.MyCommand): Found version: $($Latest.Version)."
-            $Files = ('{' + $(([Regex]::Matches($Metadata, '(?<={)(.*?)(?=}],"switch")')).Value) + '}]}' | ConvertFrom-Json).info | `
+
+            # Find the files matching this version number for this language
+            $Files = $InfoData | Where-Object { $_.Language -eq $Language } | `
                 Sort-Object -Property @{ Expression = { [System.Version]$_.Version }; Descending = $true } | `
                 Where-Object { $_.OSType -eq "windows" -and $_.Version -eq $Latest.Version }
             Write-Verbose -Message "$($MyInvocation.MyCommand): Found $($Files.Count) files."
-            $Updates = $Files | Where-Object { $_.Legacy -eq $False -and $_.OSNames -match $res.Get.Update.OS }
-        }
-        catch {
-            Write-Warning -Message "$($MyInvocation.MyCommand): $($_.Exception.Message)"
-        }
 
-        # Output the object to the pipeline
-        foreach ($Update in $Updates) {
-            $PSObject = [PSCustomObject] @{
-                Version      = $Update.Version
-                Size         = $Update.Size
-                Hash         = $Update.Hash
-                Language     = $Update.Language
-                Architecture = Get-Architecture -String $Update.Path
-                Type         = Get-FileType -File $Update.Path
-                URI          = $res.Get.Download.Uri -replace "#file", $Update.Path
+            # Filter the files for the updates we need
+            $Updates = $Files | Where-Object { $_.Legacy -eq $False -and $_.OSNames -match $res.Get.Update.OS }
+            Write-Verbose -Message "$($MyInvocation.MyCommand): Found $($Updates.Count) individual update files."
+
+            # Output the object to the pipeline
+            foreach ($Update in $Updates) {
+                $PSObject = [PSCustomObject] @{
+                    Version      = $Update.Version
+                    Size         = $Update.Size
+                    Hash         = $Update.Hash
+                    Language     = $Update.Language
+                    Architecture = Get-Architecture -String $Update.Path
+                    Type         = Get-FileType -File $Update.Path
+                    URI          = $res.Get.Download.Uri -replace "#file", $Update.Path
+                }
+                Write-Output -InputObject $PSObject
             }
-            Write-Output -InputObject $PSObject
         }
     }
 }
