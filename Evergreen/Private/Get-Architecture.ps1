5bf84eaaf79ab534ea586681b3ffc84a90844efa
Aaron Parker <aaron@stealthpuppy.com>
2023-05-13 11:35:39 +1000

    Fix swapped code in functions
    #489

--- Evergreen/Private/Get-Architecture.ps1
+++ Evergreen/Private/Get-Architecture.ps1
@@ -1,21 +1,41 @@
-function Get-InstallerType {
+function Get-Architecture {
     [OutputType([System.String])]
-    [CmdletBinding(SupportsShouldProcess = $false)]
+    [CmdletBinding(SupportsShouldProcess = $False)]
     param (
-        [Parameter(Mandatory = $true, Position = 0)]
-        #[ValidateNotNullOrEmpty()]
+        [Parameter(Mandatory = $True, Position = 0)]
         [System.String] $String
     )
 
     switch -Regex ($String.ToLower()) {
-        "user" { $Type = "User"; break }
-        "portable" { $Type = "Portable"; break }
-        "no-installer" { $Type = "Portable"; break }
-        "debug" { $Type = "Debug"; break }
+        "aarch64"   { $architecture = "ARM64"; break }
+        "amd64"     { $architecture = "AMD64"; break }
+        "arm64"     { $architecture = "ARM64"; break }
+        "arm32"     { $architecture = "ARM32"; break }
+        "arm"       { $architecture = "ARM32"; break }
+        "nt64"      { $architecture = "x64"; break }
+        "nt32"      { $architecture = "x86"; break }
+        "win64"     { $architecture = "x64"; break }
+        "win32"     { $architecture = "x86"; break }
+        "win-arm64" { $architecture = "ARM64"; break }
+        "win-x64"   { $architecture = "x64"; break }
+        "win-x86"   { $architecture = "x86"; break }
+        "x86_64"    { $architecture = "x64"; break }
+        "x64"       { $architecture = "x64"; break }
+        "w64"       { $architecture = "x64"; break }
+        "-64"       { $architecture = "x64"; break }
+        "64-bit"    { $architecture = "x64"; break }
+        "64bit"     { $architecture = "x64"; break }
+        "32-bit"    { $architecture = "x86"; break }
+        "32bit"     { $architecture = "x86"; break }
+        "x32"       { $architecture = "x86"; break }
+        "w32"       { $architecture = "x86"; break }
+        "-32"       { $architecture = "x86"; break }
+        "-x86"      { $architecture = "x86"; break }
+        "x86"       { $architecture = "x86"; break }
         default {
-            Write-Verbose -Message "$($MyInvocation.MyCommand): Installer type not found in $String, defaulting to 'Default'."
-            $Type = "Default"
+            Write-Verbose -Message "$($MyInvocation.MyCommand): Architecture not found in $String, defaulting to x86."
+            $architecture = "x86"
         }
     }
-    Write-Output -InputObject $Type
+    Write-Output -InputObject $architecture
 }
--- Evergreen/Private/Get-InstallerType.ps1
+++ Evergreen/Private/Get-InstallerType.ps1
@@ -1,42 +1,20 @@
-function Get-Architecture {
+function Get-InstallerType {
     [OutputType([System.String])]
-    [CmdletBinding(SupportsShouldProcess = $False)]
+    [CmdletBinding(SupportsShouldProcess = $false)]
     param (
-        [Parameter(Mandatory = $True, Position = 0)]
-        #[ValidateNotNullOrEmpty()]
+        [Parameter(Mandatory = $true, Position = 0)]
         [System.String] $String
     )
 
     switch -Regex ($String.ToLower()) {
-        "aarch64" { $architecture = "ARM64"; break }
-        "amd64" { $architecture = "AMD64"; break }
-        "arm64" { $architecture = "ARM64"; break }
-        "arm32" { $architecture = "ARM32"; break }
-        "arm" { $architecture = "ARM32"; break }
-        "nt64" { $architecture = "x64"; break }
-        "nt32" { $architecture = "x86"; break }
-        "win64" { $architecture = "x64"; break }
-        "win32" { $architecture = "x86"; break }
-        "win-arm64" { $architecture = "ARM64"; break }
-        "win-x64" { $architecture = "x64"; break }
-        "win-x86" { $architecture = "x86"; break }
-        "x86_64" { $architecture = "x64"; break }
-        "x64" { $architecture = "x64"; break }
-        "w64" { $architecture = "x64"; break }
-        "-64" { $architecture = "x64"; break }
-        "64-bit" { $architecture = "x64"; break }
-        "64bit" { $architecture = "x64"; break }
-        "32-bit" { $architecture = "x86"; break }
-        "32bit" { $architecture = "x86"; break }
-        "x32" { $architecture = "x86"; break }
-        "w32" { $architecture = "x86"; break }
-        "-32" { $architecture = "x86"; break }
-        "-x86" { $architecture = "x86"; break }
-        "x86" { $architecture = "x86"; break }
+        "user"          { $Type = "User"; break }
+        "portable"      { $Type = "Portable"; break }
+        "no-installer"  { $Type = "Portable"; break }
+        "debug"         { $Type = "Debug"; break }
         default {
-            Write-Verbose -Message "$($MyInvocation.MyCommand): Architecture not found in $String, defaulting to x86."
-            $architecture = "x86"
+            Write-Verbose -Message "$($MyInvocation.MyCommand): Installer type not found in $String, defaulting to 'Default'."
+            $Type = "Default"
         }
     }
-    Write-Output -InputObject $architecture
+    Write-Output -InputObject $Type
 }
