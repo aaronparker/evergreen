c39151e6374c278e28b4831909645b2ce093ddfb
Aaron Parker <aaron@stealthpuppy.com>
2023-08-25 21:11:20 +1000

    Update Get-OpenLens.ps1
    Update code formatting

--- Evergreen/Apps/Get-OpenLens.ps1
+++ Evergreen/Apps/Get-OpenLens.ps1
@@ -8,7 +8,7 @@ Function Get-OpenLens {
             Author: BornToBeRoot
     #>
     [OutputType([System.Management.Automation.PSObject])]
-    [Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSUseSingularNouns", "", Justification="Product name is a plural")]
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSUseSingularNouns", "", Justification = "Product name is a plural")]
     [CmdletBinding(SupportsShouldProcess = $False)]
     param (
         [Parameter(Mandatory = $False, Position = 0)]
@@ -28,12 +28,10 @@ Function Get-OpenLens {
     # For windows there are two different .exe versions.
     # Setup:    OpenLens.Setup.6.5.2-366.exe
     # Portable: OpenLens.6.5.2-366.exe
-    foreach($o in $object) {
-        if(-not($o.URI.contains("Setup")) -and $o.URI.EndsWith(".exe"))
-        {
+    foreach ($o in $object) {
+        if (-not($o.URI.contains("Setup")) -and $o.URI.EndsWith(".exe")) {
             $o.InstallerType = "Portable"
         }
     }
-
     Write-Output -InputObject $object
 }
