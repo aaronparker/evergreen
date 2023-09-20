63ce08f148472436c0098e6146902bb89fdc48dd
Aaron Parker <aaron@stealthpuppy.com>
2023-09-20 13:50:05 +1000

    #511
    Add replace ".windows" in version tag to return a proper version number. Return .exe installers only to simplify returning installer downloads (remove .zip installers that return mingit and busybox versions)

--- Evergreen/Apps/Get-GitForWindows.ps1
+++ Evergreen/Apps/Get-GitForWindows.ps1
@@ -1,4 +1,4 @@
-Function Get-GitForWindows {
+function Get-GitForWindows {
     <#
         .SYNOPSIS
             Returns the available Git for Windows versions.
@@ -8,20 +8,21 @@ Function Get-GitForWindows {
             Twitter: @xenappblog
     #>
     [OutputType([System.Management.Automation.PSObject])]
-    [Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSUseSingularNouns", "", Justification="Product name is a plural")]
-    [CmdletBinding(SupportsShouldProcess = $False)]
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSUseSingularNouns", "", Justification = "Product name is a plural")]
+    [CmdletBinding(SupportsShouldProcess = $false)]
     param (
-        [Parameter(Mandatory = $False, Position = 0)]
-        [ValidateNotNull()]
+        [Parameter(Mandatory = $false, Position = 0)]
+        [ValidateNotNullOrEmpty()]
         [System.Management.Automation.PSObject]
         $res = (Get-FunctionResource -AppName ("$($MyInvocation.MyCommand)".Split("-"))[1])
     )
 
     # Pass the repo releases API URL and return a formatted object
     $params = @{
-        Uri          = $res.Get.Uri
-        MatchVersion = $res.Get.MatchVersion
-        Filter       = $res.Get.MatchFileTypes
+        Uri            = $res.Get.Uri
+        MatchVersion   = $res.Get.MatchVersion
+        VersionReplace = $res.Get.VersionReplace
+        Filter         = $res.Get.MatchFileTypes
     }
     $object = Get-GitHubRepoRelease @params
     Write-Output -InputObject $object
