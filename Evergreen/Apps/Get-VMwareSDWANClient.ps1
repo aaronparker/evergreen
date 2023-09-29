42a97bc85f2a1f2bac859ba3b813a2e19dc90905
Aaron Parker <aaron@stealthpuppy.com>
2023-09-29 19:25:32 +1000

    Add VMwareSDWANClient

---
+++ Evergreen/Apps/Get-VMwareSDWANClient.ps1
@@ -0,0 +1,23 @@
+function Get-VMwareSDWANClient {
+    <#
+        .NOTES
+            Author: Aaron Parker
+            Twitter: @stealthpuppy
+    #>
+    [OutputType([System.Management.Automation.PSObject])]
+    [CmdletBinding(SupportsShouldProcess = $False)]
+    param (
+        [Parameter(Mandatory = $false, Position = 0)]
+        [ValidateNotNull()]
+        [System.Management.Automation.PSObject]
+        $res = (Get-FunctionResource -AppName ("$($MyInvocation.MyCommand)".Split("-"))[1])
+    )
+
+    $Output = Get-VMwareProductList -Name $res.Get.Download.ProductName | `
+        Get-VMwareProductDownload | `
+        Where-Object { $_.URI -match $res.Get.Download.MatchFileTypes } | `
+        Sort-Object -Property "ReleaseDate" | `
+        Select-Object -Last 1 | `
+        ForEach-Object { $_.Version = $_.Version -replace $res.Get.Download.ReplaceText, ""; $_ }
+    Write-Output -InputObject $Output
+}
