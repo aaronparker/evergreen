05d453a05dda994ca51ac5cbbdb061f940e7b99a
Aaron Parker <aaron@stealthpuppy.com>
2023-09-30 22:09:15 +1000

    Add ZoomVDI
    #555 #556

--- Evergreen/Apps/Get-Zoom.ps1
+++ Evergreen/Apps/Get-Zoom.ps1
@@ -4,8 +4,8 @@ function Get-Zoom {
             Get the current version and download URL for Zoom.
 
         .NOTES
-            Author: Trond Eirik Haavarstein
-            Twitter: @xenappblog
+            Author: Aaron Parker
+            Twitter: @stealthpuppy
     #>
     [OutputType([System.Management.Automation.PSObject])]
     [CmdletBinding(SupportsShouldProcess = $False)]
@@ -15,48 +15,6 @@ function Get-Zoom {
         [System.Management.Automation.PSObject]
         $res = (Get-FunctionResource -AppName ("$($MyInvocation.MyCommand)".Split("-"))[1])
     )
-
-    <#
-    foreach ($platform in $res.Get.Download.Keys) {
-        foreach ($installer in $res.Get.Download[$platform].Keys) {
-
-            # Follow the download link which will return a 301/302
-            $redirectUrl = Resolve-SystemNetWebRequest -Uri $res.Get.Download[$platform][$installer]
-
-            # Match the URL without the text after the ?
-            if ($null -eq $redirectUrl) {
-                Write-Verbose -Message "$($MyInvocation.MyCommand): Setting fallback URL to: $($script:resourceStrings.Uri.Issues)."
-                $Url = $script:resourceStrings.Uri.Issues
-            }
-            else {
-                try {
-                    $Url = [RegEx]::Match($redirectUrl.ResponseUri.AbsoluteUri, $res.Get.MatchUrl).Captures.Groups[1].Value
-                }
-                catch {
-                    $Url = $redirectUrl.ResponseUri.AbsoluteUri
-                }
-            }
-
-            # Match version number from the download URL
-            try {
-                $Version = [RegEx]::Match($Url, $res.Get.MatchVersion).Captures.Groups[0].Value
-            }
-            catch {
-                $Version = "Latest"
-            }
-
-            # Construct the output; Return the custom object to the pipeline
-            $PSObject = [PSCustomObject] @{
-                Version      = $Version
-                Platform     = $platform
-                Type         = Get-FileType -File $Url
-                Architecture = Get-Architecture -String $Url
-                URI          = $Url
-            }
-            Write-Output -InputObject $PSObject
-        }
-    }
-    #>
 
     # Get the download data from the API
     $params = @{
@@ -88,7 +46,7 @@ function Get-Zoom {
         }
 
         # Create an output object
-        [PSCustomObject]@{
+        $Output = [PSCustomObject]@{
             Version      = $Version
             Platform     = $res.Get.Download.PropertyMatrix[$Property]
             Installer    = "User"
@@ -97,6 +55,7 @@ function Get-Zoom {
             Architecture = Get-Architecture -String $ResolvedUrl.ResponseUri.AbsoluteUri
             URI          = $ResolvedUrl.ResponseUri.AbsoluteUri
         }
+        Write-Output -InputObject $Output
 
         # Construct the URL for the IT installer
         if (-not([System.String]::IsNullOrEmpty($DownloadFeed.result.downloadVO.$Property.packageNameForIT))) {
@@ -111,7 +70,7 @@ function Get-Zoom {
             $ResolvedUrl = Resolve-SystemNetWebRequest -Uri $Url
 
             # Create an output object
-            [PSCustomObject]@{
+            $Output = [PSCustomObject]@{
                 Version      = $Version
                 Platform     = $res.Get.Download.PropertyMatrix[$Property]
                 Installer    = "Admin"
@@ -120,6 +79,7 @@ function Get-Zoom {
                 Architecture = Get-Architecture -String $ResolvedUrl.ResponseUri.AbsoluteUri
                 URI          = $ResolvedUrl.ResponseUri.AbsoluteUri
             }
+            Write-Output -InputObject $Output
         }
     }
 }
