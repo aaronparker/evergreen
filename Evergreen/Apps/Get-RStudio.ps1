5658b415647097562715a9b4c44c92a09a0ba3b2
Aaron Parker <aaron@stealthpuppy.com>
2023-10-01 10:24:29 +1100

    Update RStudio
    #554

--- Evergreen/Apps/Get-RStudio.ps1
+++ Evergreen/Apps/Get-RStudio.ps1
@@ -1,4 +1,4 @@
-Function Get-RStudio {
+function Get-RStudio {
     <#
         .SYNOPSIS
             Returns the available RStudio version and download URI.
@@ -9,48 +9,31 @@ Function Get-RStudio {
             Based on Get-AtlassianBitbucket.ps1
     #>
     [OutputType([System.Management.Automation.PSObject])]
-    [CmdletBinding(SupportsShouldProcess = $False)]
+    [CmdletBinding(SupportsShouldProcess = $false)]
     param (
-        [Parameter(Mandatory = $False, Position = 0)]
+        [Parameter(Mandatory = $false, Position = 0)]
         [ValidateNotNull()]
         [System.Management.Automation.PSObject]
         $res = (Get-FunctionResource -AppName ("$($MyInvocation.MyCommand)".Split("-"))[1])
     )
 
-    # Read the update URI
-    foreach ($branch in $res.Get.Update.Uri.GetEnumerator()) {
-        $params = @{
-            Uri = $res.Get.Update.Uri[$branch.Key]
-        }
-        $Content = Invoke-RestMethodWrapper @params
+    # Read the download URI
+    $Content = Invoke-RestMethodWrapper -Uri $res.Get.Download.Uri
 
-        # Read the JSON and build an array of platform, channel, version
-        If ($Null -ne $Content) {
+    # Step through each installer type
+    foreach ($Product in $res.Get.Download.Products) {
 
-            # Step through each installer type
-            foreach ($product in $res.Get.Update.Products) {
-                foreach ($platform in $res.Get.Update.Platforms) {
-                    foreach ($item in $Content.$product.platforms.$platform) {
-
-                        # Build the output object; Output object to the pipeline
-                        $PSObject = [PSCustomObject] @{
-                            Version       = $item.version
-                            Sha256        = $item.sha256
-                            Size          = $item.size
-                            Branch        = $branch.Name
-                            Channel       = $item.channel
-                            ProductName   = $Content.$product.name
-                            InstallerName = $item.name
-                            Type          = Get-FileType -File $item.link
-                            URI           = $item.link
-                        }
-                        Write-Output -InputObject $PSObject
-                    }
-                }
-            }
+        # Build the output object; Output object to the pipeline
+        $PSObject = [PSCustomObject] @{
+            Version      = $Content.rstudio.$Product.stable.desktop.installer.windows.version
+            ProductName  = $Content.rstudio.$Product.stable.desktop.installer.windows.label
+            Pro          = $Content.rstudio.$Product.stable.desktop.installer.windows.pro
+            Date         = $Content.rstudio.$Product.stable.desktop.installer.windows.last_modified
+            Size         = $Content.rstudio.$Product.stable.desktop.installer.windows.size
+            Sha256       = $Content.rstudio.$Product.stable.desktop.installer.windows.sha256
+            Type         = Get-FileType -File $Content.rstudio.$Product.stable.desktop.installer.windows.url
+            URI          = $Content.rstudio.$Product.stable.desktop.installer.windows.url
         }
-        else {
-            Write-Error -Message "$($MyInvocation.MyCommand): Unable to return usable content from: $($res.Get.Update.Uri[$branch.Key])."
-        }
+        Write-Output -InputObject $PSObject
     }
 }
