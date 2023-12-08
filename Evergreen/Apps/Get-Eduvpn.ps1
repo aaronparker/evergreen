87ba09cf79cbfb3f95a49b530f576cd0c689aa35
Jasper Metselaar <82034069+jms-du-se@users.noreply.github.com>
2023-12-08 14:14:20 +0100

    Adding Get script for eduVPN

--- Evergreen/Apps/Get-Eduvpn.ps1
+++ Evergreen/Apps/Get-Eduvpn.ps1
@@ -23,8 +23,6 @@ Function Get-eduVPN {
         Filter       = $res.Get.MatchFileTypes
     }
     $object = Get-GitHubRepoRelease @params
-    #Filter the LetsConnectClient installers
-    $object = $object | Where-Object {$_ -notmatch "LetsConnectClient_"}
 
     Write-Output -InputObject $object
 }
