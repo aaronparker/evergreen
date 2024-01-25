7c3e85f90a06ce94ae76042fada92cb7692db125
Jasper Metselaar <jms@du.se>
2024-01-25 11:23:45 +0100

    New Application: DB Browser for SQLite

---
+++ Evergreen/Apps/Get-DBBrowserforSQLite.ps1
@@ -0,0 +1,28 @@
+Function Get-DBBrowserforSQLite {
+    <#
+        .SYNOPSIS
+            Returns the available DB Browser for SQLite versions.
+
+        .NOTES
+            Author: Jasper Metselaar
+            E-mail: jms@du.se
+    #>
+    [OutputType([System.Management.Automation.PSObject])]
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSUseSingularNouns", "", Justification="Product name is a plural")]
+    [CmdletBinding(SupportsShouldProcess = $False)]
+    param (
+        [Parameter(Mandatory = $False, Position = 0)]
+        [ValidateNotNull()]
+        [System.Management.Automation.PSObject]
+        $res = (Get-FunctionResource -AppName ("$($MyInvocation.MyCommand)".Split("-"))[1])
+    )
+
+    # Pass the repo releases API URL and return a formatted object
+    $params = @{
+        Uri          = $res.Get.Uri
+        MatchVersion = $res.Get.MatchVersion
+        Filter       = $res.Get.MatchFileTypes
+    }
+    $object = Get-GitHubRepoRelease @params
+    Write-Output -InputObject $object
+}
