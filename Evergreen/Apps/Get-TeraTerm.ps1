1226b909457e66d644408ddeea8567883479a412
Jasper Metselaar <jms@du.se>
2024-03-12 14:10:51 +0100

    New App: Tera Term

---
+++ Evergreen/Apps/Get-TeraTerm.ps1
@@ -0,0 +1,27 @@
+Function Get-TeraTerm {
+    <#
+        .SYNOPSIS
+            Returns the available TeraTerm versions.
+
+        .NOTES
+            Author: Jasper Metselaar
+            E-mail: jms@du.se
+    #>
+    [OutputType([System.Management.Automation.PSObject])]
+    [CmdletBinding(SupportsShouldProcess = $False)]
+    param (
+        [Parameter(Mandatory = $False, Position = 0)]
+        [ValidateNotNull()]
+        [System.Management.Automation.PSObject]
+        $res = (Get-FunctionResource -AppName ("$($MyInvocation.MyCommand)".Split("-"))[1])
+    )
+
+    # Pass the repo releases API URL and return a formatted object
+    $params = @{
+        Uri          = $res.Get.Uri
+        MatchVersion = $res.Get.MatchVersion
+        Filter       = $res.Get.MatchFileTypes
+    }
+    $object = Get-GitHubRepoRelease @params
+    Write-Output -InputObject $object
+}
