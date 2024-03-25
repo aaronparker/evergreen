cb37b55587f2aecbf816a32d7d22e6c4f6770dee
Aaron Parker <aaron@stealthpuppy.com>
2024-03-25 22:31:17 +1100

    Add Plex apps #270
    PlexDesktop, PlexMediaServer, PlexHTPC

---
+++ Evergreen/Apps/Get-PlexMediaServer.ps1
@@ -0,0 +1,32 @@
+function Get-PlexMediaServer {
+    <#
+        .NOTES
+            Author: Aaron Parker
+            Twitter: @stealthpuppy
+    #>
+    [OutputType([System.Management.Automation.PSObject])]
+    [CmdletBinding(SupportsShouldProcess = $False)]
+    param (
+        [Parameter(Mandatory = $False, Position = 0)]
+        [ValidateNotNull()]
+        [System.Management.Automation.PSObject]
+        $res = (Get-FunctionResource -AppName ("$($MyInvocation.MyCommand)".Split("-"))[1])
+    )
+
+    # Get the list of downloads
+    $params = @{
+        Uri = $res.Get.Download.Uri
+    }
+    $Content = Invoke-EvergreenRestMethod @params
+
+    # Return a releases object to the pipeline
+    foreach ($Release in $Content.computer.Windows.releases) {
+        $PSObject = [PSCustomObject] @{
+            Version      = $Content.computer.Windows.version
+            Architecture = Get-Architecture -String $Release.build
+            Checksum     = $Release.checksum
+            URI          = $Release.url
+        }
+        Write-Output -InputObject $PSObject
+    }
+}
