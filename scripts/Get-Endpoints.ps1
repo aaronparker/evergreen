10a74045e82d7235b8a89c1f0acd92b13a19bdde
Aaron Parker <aaron@stealthpuppy.com>
2024-04-14 21:56:20 +1000

    Update Get-Endpoints.ps1

--- scripts/Get-Endpoints.ps1
+++ scripts/Get-Endpoints.ps1
@@ -1,10 +1,14 @@
-function Get-ApplicationEndpoint {
+# function Get-ApplicationEndpoint {
     <#
         Retrieve the list of endpoints for a given application from the Evergreen API
         for input into firewall rules
     #>
-    [Parameter()]
-    [System.String[]] $Apps = @("MicrosoftWvdRtcService",
+[CmdletBinding(SupportsShouldProcess = $true)]
+param (
+    [Parameter(Position = 0,
+        ValueFromPipeline)]
+    [System.String[]] $Apps = @(
+        "MicrosoftWvdRtcService",
         "MicrosoftWvdMultimediaRedirection",
         "MicrosoftFSLogixApps",
         "Microsoft.NET",
@@ -14,12 +18,16 @@ function Get-ApplicationEndpoint {
         "Microsoft365Apps",
         "AdobeAcrobatReaderDC",
         "RemoteDisplayAnalyzer")
+)
 
+begin {
     $Versions = Invoke-RestMethod -Uri "https://evergreen-api.stealthpuppy.com/endpoints/versions"
-    $Versions | Where-Object { $_.Application -in $Apps } | Select-Object -ExpandProperty "Endpoints" -Unique | Write-Output
-
     $Downloads = Invoke-RestMethod -Uri "https://evergreen-api.stealthpuppy.com/endpoints/downloads"
+}
+process {
+    $Versions | Where-Object { $_.Application -in $Apps } | Select-Object -ExpandProperty "Endpoints" -Unique | Write-Output
     $Downloads | Where-Object { $_.Application -in $Apps } | Select-Object -ExpandProperty "Endpoints" -Unique | Write-Output
 }
+# }
 
-Get-ApplicationEndpoint | Select-Object -Unique
+# Get-ApplicationEndpoint | Select-Object -Unique
