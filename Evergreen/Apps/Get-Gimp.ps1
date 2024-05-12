f1614ef41f344fd923f5310efe9905e194bdf03c
Aaron Parker <aaron@stealthpuppy.com>
2024-05-12 22:17:21 +1000

    #639
    Add Revision property, add Stable and Development channels

--- Evergreen/Apps/Get-Gimp.ps1
+++ Evergreen/Apps/Get-Gimp.ps1
@@ -1,4 +1,4 @@
-Function Get-Gimp {
+function Get-Gimp {
     <#
         .SYNOPSIS
             Get the current version and download URL for GIMP.
@@ -9,9 +9,9 @@ Function Get-Gimp {
             Twitter: @stealthpuppy
     #>
     [OutputType([System.Management.Automation.PSObject])]
-    [CmdletBinding(SupportsShouldProcess = $False)]
+    [CmdletBinding(SupportsShouldProcess = $false)]
     param (
-        [Parameter(Mandatory = $False, Position = 0)]
+        [Parameter(Mandatory = $false, Position = 0)]
         [ValidateNotNull()]
         [System.Management.Automation.PSObject]
         $res = (Get-FunctionResource -AppName ("$($MyInvocation.MyCommand)".Split("-"))[1])
@@ -21,36 +21,42 @@ Function Get-Gimp {
     $updateFeed = Invoke-EvergreenRestMethod -Uri $res.Get.Update.Uri
     if ($null -ne $updateFeed) {
 
-        # Grab latest version, sort by descending version number
-        $Latest = $updateFeed.STABLE | `
-            Sort-Object -Property @{ Expression = { [System.Version]$_.version }; Descending = $true } | `
-            Select-Object -First 1
-        $MinorVersion = [System.Version] $Latest.version
+        foreach ($Channel in $res.Get.Update.Channels) {
 
-        if ($null -ne $Latest) {
-            # Grab the latest Windows release, sort by descending date
-            $LatestWin = $Latest.windows | `
-                Sort-Object -Property @{ Expression = { [System.DateTime]::ParseExact($_.date, "yyyy-MM-dd", $Null) }; Descending = $true } | `
+            # Grab latest version, sort by descending version number
+            Write-Verbose -Message "$($MyInvocation.MyCommand): Channel: $Channel."
+            $Latest = $updateFeed.$Channel | `
+                Sort-Object -Property @{ Expression = { [System.Version]$_.version }; Descending = $true } | `
                 Select-Object -First 1
+            $MinorVersion = [System.Version] $Latest.version
 
-            if ($null -ne $LatestWin) {
+            if ($null -ne $Latest) {
+                # Grab the latest Windows Channel, sort by descending date
+                $LatestWin = $Latest.windows | `
+                    Sort-Object -Property @{ Expression = { [System.DateTime]::ParseExact($_.date, "yyyy-MM-dd", $null) }; Descending = $true } | `
+                    Select-Object -First 1
 
-                # Build the download URL
-                $Uri = ($res.Get.Download.Uri -replace $res.Get.Download.ReplaceFileName, $LatestWin.filename) -replace $res.Get.Download.ReplaceVersion, "$($MinorVersion.Major).$($MinorVersion.Minor)"
+                if ($null -ne $LatestWin) {
 
-                # Follow the download link which will return a 301/302
-                Write-Verbose -Message "$($MyInvocation.MyCommand): Resolving: $Uri."
-                $redirectUrl = Resolve-InvokeWebRequest -Uri $Uri
+                    # Build the download URL
+                    $Uri = ($res.Get.Download.Uri -replace $res.Get.Download.ReplaceFileName, $LatestWin.filename) -replace $res.Get.Download.ReplaceVersion, "$($MinorVersion.Major).$($MinorVersion.Minor)"
 
-                # Construct the output; Return the custom object to the pipeline
-                if ($null -ne $redirectUrl) {
-                    $PSObject = [PSCustomObject] @{
-                        Version = $Latest.version
-                        Date    = ConvertTo-DateTime -DateTime $LatestWin.date -Pattern $res.Get.Update.DatePattern
-                        Sha256  = $LatestWin.sha256
-                        URI     = $redirectUrl
+                    # Follow the download link which will return a 301/302
+                    Write-Verbose -Message "$($MyInvocation.MyCommand): Resolving: $Uri."
+                    $redirectUrl = Resolve-InvokeWebRequest -Uri $Uri
+
+                    # Construct the output; Return the custom object to the pipeline
+                    if ($null -ne $redirectUrl) {
+                        $PSObject = [PSCustomObject] @{
+                            Version  = $Latest.version
+                            Revision = if ([System.String]::IsNullOrEmpty($Latest.windows.revision)) { "0" } else { $Latest.windows.revision }
+                            Channel  = (Get-Culture).TextInfo.ToTitleCase($Channel.ToLower())
+                            Date     = ConvertTo-DateTime -DateTime $LatestWin.date -Pattern $res.Get.Update.DatePattern
+                            Sha256   = $LatestWin.sha256
+                            URI      = $redirectUrl
+                        }
+                        Write-Output -InputObject $PSObject
                     }
-                    Write-Output -InputObject $PSObject
                 }
             }
         }
