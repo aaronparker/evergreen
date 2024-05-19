e1c9108066d61b5553017bf2010a19efdbc61c0f
Aaron Parker <aaron@stealthpuppy.com>
2024-05-19 13:22:31 +1000

    Add RedHatOpenJDK21

---
+++ Evergreen/Apps/Get-RedHatOpenJDK21.ps1
@@ -0,0 +1,18 @@
+function Get-RedHatOpenJDK21 {
+    <#
+        .NOTES
+            Author: Aaron Parker
+            Twitter: @stealthpuppy
+    #>
+    [OutputType([System.Management.Automation.PSObject])]
+    [CmdletBinding(SupportsShouldProcess = $False)]
+    param (
+        [Parameter(Mandatory = $false, Position = 0)]
+        [ValidateNotNull()]
+        [System.Management.Automation.PSObject]
+        $res = (Get-FunctionResource -AppName ("$($MyInvocation.MyCommand)".Split("-"))[1])
+    )
+
+    $Output = Get-AdoptiumTemurin -res $res
+    Write-Output -InputObject $Output
+}
