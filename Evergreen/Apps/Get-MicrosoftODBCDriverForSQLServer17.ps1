248da898ed284e025c67c5f9d8e07d2ff40ff54e
Aaron Parker <aaron@stealthpuppy.com>
2024-05-19 18:31:20 +1000

    #685
    Add MicrosoftODBCDriverForSQLServer17

---
+++ Evergreen/Apps/Get-MicrosoftODBCDriverForSQLServer17.ps1
@@ -0,0 +1,28 @@
+function Get-MicrosoftODBCDriverForSQLServer17 {
+    <#
+        .SYNOPSIS
+
+    #>
+    [OutputType([System.Management.Automation.PSObject])]
+    [CmdletBinding(SupportsShouldProcess = $False)]
+    param (
+        [Parameter(Mandatory = $False, Position = 0)]
+        [ValidateNotNull()]
+        [System.Management.Automation.PSObject]
+        $res = (Get-FunctionResource -AppName ("$($MyInvocation.MyCommand)".Split("-"))[1])
+    )
+
+    foreach ($language in $res.Get.Download.Language.GetEnumerator()) {
+        foreach ($Url in $res.Get.Download.Uri) {
+
+            # Construct the URL to include the language
+            $Query = "&clcid="
+            $Uri = "$($Url)$($Query)$($res.Get.Download.Language[$language.key])"
+            $params = @{
+                Uri                = $Uri
+                MaximumRedirection = $res.Get.Download.MaximumRedirection
+            }
+            Resolve-MicrosoftFwLink @params | ForEach-Object { $_.Language = $language.key; $_ } | Write-Output
+        }
+    }
+}
