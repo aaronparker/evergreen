62b8dcbcaa969a6ac1f6d2b7c91c372433974dd6
Aaron Parker <aaron@stealthpuppy.com>
2024-07-01 09:47:26 +1000

    Remove MicrosoftOutlook
    Unable to find update API for the latest version v2

--- Disabled/Get-MicrosoftOutlook.ps1
+++ Disabled/Get-MicrosoftOutlook.ps1
@@ -16,26 +16,37 @@ function Get-MicrosoftOutlook {
         $res = (Get-FunctionResource -AppName ("$($MyInvocation.MyCommand)".Split("-"))[1])
     )
 
-    # Read the JSON and convert to a PowerShell object. Return the release version of Teams
+    # Read the update feed
     $params = @{
         Uri = $res.Get.Update.Uri
-        Raw = $True
+        Raw = $true
     }
     $Update = Invoke-EvergreenWebRequest @params
-
-    # Read the JSON and build an array of platform, channel, version
-    if ($Null -ne $Update) {
+    if ($null -ne $Update) {
 
         # Match version number
         $Version = [RegEx]::Match($Update[-1].Split(" ")[1], $res.Get.Update.MatchVersion).Captures.Groups[1].Value
         Write-Verbose -Message "$($MyInvocation.MyCommand): Found version: $Version."
 
         # Build the output object and output object to the pipeline
+        $Url = $res.Get.Download.Uri.Zip -replace "#installer", $Update[-1].Split(" ")[1]
         $PSObject = [PSCustomObject] @{
             Version  = $Version
             Sha1Hash = $Update[-1].Split(" ")[0]
             Size     = $Update[-1].Split(" ")[2]
-            URI      = $res.Get.Download.Uri -replace "#installer", $Update[-1].Split(" ")[1]
+            Type     = Get-FileType -File $Url
+            URI      = $Url
+        }
+        Write-Output -InputObject $PSObject
+
+        # Build the output object for setup.exe
+        $Url = (Resolve-SystemNetWebRequest -Uri $res.Get.Download.Uri.Exe).ResponseUri.AbsoluteUri
+        $PSObject = [PSCustomObject] @{
+            Version  = $Version
+            Sha1Hash = "Unknown"
+            Size     = "Unknown"
+            Type     = Get-FileType -File $Url
+            URI      = $Url
         }
         Write-Output -InputObject $PSObject
     }
