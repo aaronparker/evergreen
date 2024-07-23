03b0dc7412a8c243f58140120b92cb47b7cf21e1
Kirill Trofimov <work@g3rhard.cc>
2024-07-23 13:59:55 +0200

    OBS Studio: Switching to GitHub Releases

--- Evergreen/Apps/Get-OBSStudio.ps1
+++ Evergreen/Apps/Get-OBSStudio.ps1
@@ -8,8 +8,9 @@
             Author: Aaron Parker
             Twitter: @stealthpuppy
     #>
-    [CmdletBinding(SupportsShouldProcess = $False)]
     [OutputType([System.Management.Automation.PSObject])]
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSUseSingularNouns", "", Justification="Product name is a plural")]
+    [CmdletBinding(SupportsShouldProcess = $False)]
     param (
         [Parameter(Mandatory = $False, Position = 0)]
         [ValidateNotNull()]
@@ -17,36 +18,12 @@
         $res = (Get-FunctionResource -AppName ("$($MyInvocation.MyCommand)".Split("-"))[1])
     )
 
-    # Query the update feed
+    # Pass the repo releases API URL and return a formatted object
     $params = @{
-        Uri         = $res.Get.Update.Uri
-        ContentType = $res.Get.Update.ContentType
+        Uri          = $res.Get.Uri
+        MatchVersion = $res.Get.MatchVersion
+        Filter       = $res.Get.MatchFileTypes
     }
-    $Updates = Invoke-EvergreenRestMethod @params
-
-    # Output the object to the pipeline
-    If ($Null -ne $Updates) {
-        ForEach ($Update in $Updates) {
-
-            # Build the latest version number
-            If ($Update.version_patch -eq 0) {
-                # Handle edge case where binaries with version_patch of 0 are published with only major.minor version
-                $Version = "$($Update.version_major).$($Update.version_minor)"
-            } else {
-                $Version = "$($Update.version_major).$($Update.version_minor).$($Update.version_patch)"
-            }
-
-            # Build the output object
-            ForEach ($Architecture in $res.Get.Download.Architectures) {
-                $PSObject = [PSCustomObject] @{
-                    Version      = $Version
-                    Architecture = $Architecture
-                    URI          = $res.Get.Download.Uri -replace $res.Get.Download.ReplaceText.FileName, $res.Get.Download.FileName `
-                        -replace $res.Get.Download.ReplaceText.Version, $Version `
-                        -replace $res.Get.Download.ReplaceText.Architecture, $Architecture
-                }
-                Write-Output -InputObject $PSObject
-            }
-        }
-    }
+    $object = Get-GitHubRepoRelease @params
+    Write-Output -InputObject $object
 }
