71bb6252fdaace03557ecb8ee3f8a5f5f85a4a07
Aaron Parker <aaron@stealthpuppy.com>
2024-07-30 15:19:39 +1000

    Update AdobeAcrobatReaderDC
    Add x64 architecture to all languages

--- Evergreen/Apps/Get-AdobeAcrobatReaderDC.ps1
+++ Evergreen/Apps/Get-AdobeAcrobatReaderDC.ps1
@@ -22,28 +22,32 @@ Function Get-AdobeAcrobatReaderDC {
     #region Installer downloads
     foreach ($language in $res.Get.Update.Languages.GetEnumerator()) {
 
+        # Get the installer display names for the specified language
         Write-Verbose -Message "$($MyInvocation.MyCommand): Searching updates for language: $($language.Name)."
         $params = @{
             Uri = $res.Get.Update.Uri -replace "#Language", $language.Name
         }
         $UpdateContent = Invoke-EvergreenRestMethod @params
 
-        if ($Null -ne $UpdateContent) {
-            foreach ($item in $UpdateContent.products.reader) {
+        if ($null -ne $UpdateContent) {
+            foreach ($Product in $UpdateContent.products.reader) {
 
+                # Search for downloads for each display name returned for the language
                 $LanguageFullName = $($res.Get.Update.Languages[$language.Key])
                 Write-Verbose -Message "$($MyInvocation.MyCommand): Searching downloads for language: $LanguageFullName, $($language.Name)."
                 $params = @{
-                    Uri = $res.Get.Download.Uri -replace "#DisplayName", $item.displayName -replace "#ShortLanguage", $language.Name -replace " ", "%20"
+                    Uri = $res.Get.Download.Uri -replace "#DisplayName", $Product.displayName -replace "#ShortLanguage", $language.Name -replace " ", "%20"
                 }
                 $DownloadContent = Invoke-EvergreenRestMethod @params
 
+                # Build the output object
                 if ($null -ne $DownloadContent) {
                     $PSObject = [PSCustomObject] @{
-                        Version      = $item.version
+                        Version      = $Product.version
                         Language     = $LanguageFullName
+                        Size         = [System.Int32]$Product.fileSize
                         Architecture = Get-Architecture -String $DownloadContent.downloadURL
-                        Name         = $item.displayName
+                        #Name         = $Product.displayName
                         URI          = $DownloadContent.downloadURL
                     }
                     Write-Output -InputObject $PSObject
