e2539884ee27639dee4afe5bfc40f4379e1aeea1
Aaron Parker <aaron@stealthpuppy.com>
2024-09-23 12:55:00 +1000

    Update ConvertTo-DotNetVersionClass.Tests.ps1

--- tests/Public/ConvertTo-DotNetVersionClass.Tests.ps1
+++ tests/Public/ConvertTo-DotNetVersionClass.Tests.ps1
@@ -19,11 +19,11 @@ Describe -Tag "Convert" -Name "ConvertTo-DotNetVersionClass" {
     }
 
     It "Should return a valid .NET version class" {
-        { $App[0] | ConvertTo-DotNetVersionClass } | Should -BeOfType [System.Version]
+        ($App[0] | ConvertTo-DotNetVersionClass) | Should -BeOfType [System.Version]
     }
 
     It "Should return a string for a version number that fails to convert" {
-        { ConvertTo-DotNetVersionClass -Version "v22-build1" } | Should -BeOfType [System.String]
+        ConvertTo-DotNetVersionClass -Version "v22-build1" | Should -BeOfType [System.String]
     }
 
     It "Should return the expected string when converting a version string" {
