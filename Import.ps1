e798c3a68a8d84ac4cd6ca67f85dc76c60ea483b
Aaron Parker <aaron@stealthpuppy.com>
2024-09-27 09:50:40 +1000

    Update Import.ps1

--- Import.ps1
+++ Import.ps1
@@ -3,3 +3,7 @@ Import-Module ./Evergreen/Evergreen.psd1 -Force
 foreach ($file in (Get-ChildItem -Path "./Evergreen/Private/*.ps1")) {
     . $file.FullName
 }
+
+foreach ($file in (Get-ChildItem -Path "./Evergreen/Shared/*.ps1")) {
+    . $file.FullName
+}
