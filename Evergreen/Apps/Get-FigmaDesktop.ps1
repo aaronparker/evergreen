a7f478d09769c735f5f9c33f432fc79848e24e0c
Aaron Parker <aaron@stealthpuppy.com>
2024-12-13 09:00:22 +1100

    Add Figma Desktop manifest and PowerShell script for installation #784

---
+++ Evergreen/Apps/Get-FigmaDesktop.ps1
@@ -0,0 +1,25 @@
+function Get-FigmaDesktop {
+    <#
+        .NOTES
+            Site: https://stealthpuppy.com
+            Author: Aaron Parker
+    #>
+    [OutputType([System.Management.Automation.PSObject])]
+    [CmdletBinding(SupportsShouldProcess = $false)]
+    param (
+        [Parameter(Mandatory = $false, Position = 0)]
+        [ValidateNotNull()]
+        [System.Management.Automation.PSObject]
+        $res = (Get-FunctionResource -AppName ("$($MyInvocation.MyCommand)".Split("-"))[1])
+    )
+
+    $Content = Invoke-EvergreenRestMethod -Uri $res.Get.Update.Uri
+    if ($null -ne $Content) {
+        $object = [PSCustomObject]@{
+            Version = $Content.title -replace $res.Get.Update.ReplaceText, ""
+            Date    = $Content.pubDate
+            URL     = $Content.link
+        }
+        Write-Output -InputObject $object
+    }
+}
