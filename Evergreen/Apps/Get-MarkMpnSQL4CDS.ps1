e5b1dd3f76ee1a5571cb7a5350a4b703ce8372fa
Aaron Parker <aaron@stealthpuppy.com>
2025-02-22 17:15:58 +1100

    #798

---
+++ Evergreen/Apps/Get-MarkMpnSQL4CDS.ps1
@@ -0,0 +1,26 @@
+function Get-MarkMpnSQL4CDS {
+    <#
+        .SYNOPSIS
+            Returns Mark Carrington's latest MarkMpn SQL 4 CDS version number and download.
+
+        .NOTES
+            Author: Adrian Scott
+    #>
+    [OutputType([System.Management.Automation.PSObject])]
+    [CmdletBinding(SupportsShouldProcess = $false)]
+    param (
+        [Parameter(Mandatory = $false, Position = 0)]
+        [ValidateNotNull()]
+        [System.Management.Automation.PSObject]
+        $res = (Get-FunctionResource -AppName ("$($MyInvocation.MyCommand)".Split("-"))[1])
+    )
+
+    # Pass the repo releases API URL and return a formatted object
+    $params = @{
+        Uri          = $res.Get.Uri
+        MatchVersion = $res.Get.MatchVersion
+        Filter       = $res.Get.MatchFileTypes
+    }
+    $object = Get-GitHubRepoRelease @params
+    Write-Output -InputObject $object
+}
