dce9cbb02f407e9e1cdf2b2dc06cc119074d1be4
Aaron Parker <aaron@stealthpuppy.com>
2025-03-09 10:28:04 +1100

    Update Get-MicrosoftWvdRtcService.ps1
    Fix changes to source URL

--- Evergreen/Apps/Get-MicrosoftWvdRtcService.ps1
+++ Evergreen/Apps/Get-MicrosoftWvdRtcService.ps1
@@ -17,46 +17,26 @@ function Get-MicrosoftWvdRtcService {
         $res = (Get-FunctionResource -AppName ("$($MyInvocation.MyCommand)".Split("-"))[1])
     )
 
-    # Grab the download link headers to find the file name
     $params = @{
-        Uri          = $res.Get.Download.Uri
-        Method       = "Head"
-        ReturnObject = "Headers"
+        Uri = $res.Get.Download.Uri
     }
-    $Headers = Invoke-EvergreenWebRequest @params
-    if ($null -ne $Headers) {
+    $Response = Resolve-SystemNetWebRequest @params
 
-        # Match filename
-        # $Filename = [Regex]::Match($Headers['Content-Disposition'], $res.Get.Download.MatchFilename).Captures.Groups[1].Value
-
-        # Match filename
-        #$Filename = [RegEx]::Match($Headers.'Content-Disposition', $res.Get.Download.MatchFilename).Captures.Groups[1].Value
-        $FileName = $Headers.'Content-Disposition'.Split("=")[-1]
-        if ($null -ne $Filename) {
-            Write-Verbose -Message "$($MyInvocation.MyCommand): Found filename: $Filename."
-        }
-        else {
-            Write-Warning -Message "$($MyInvocation.MyCommand): Failed to match filename from '$($Headers.'Content-Disposition')'."
-        }
-
-        # Match version
-        $Version = [RegEx]::Match($FileName, $res.Get.Download.MatchVersion).Captures.Groups[1].Value
-        if ($null -ne $Version) {
-            Write-Verbose -Message "$($MyInvocation.MyCommand): Found version: $Version."
-        }
-        else {
-            $Version = [RegEx]::Match($Headers.'Content-Disposition', $res.Get.Download.MatchVersionFallback).Captures.Groups[1].Value
-            Write-Verbose -Message "$($MyInvocation.MyCommand): Found version: $Version."
-        }
-
-        # Construct the output; Return the custom object to the pipeline
-        $PSObject = [PSCustomObject] @{
-            Version      = $Version
-            Date         = ConvertTo-DateTime -DateTime $($Headers.'Last-Modified' | Select-Object -First 1) -Pattern $res.Get.Download.DateFormat
-            Architecture = Get-Architecture -String $Filename
-            Filename     = $Filename
-            URI          = $res.Get.Download.Uri
-        }
-        Write-Output -InputObject $PSObject
+    # Match version
+    $Version = [RegEx]::Match($Response.ResponseUri.AbsoluteUri, $res.Get.Download.MatchVersion).Captures.Groups[1].Value
+    if ($null -ne $Version) {
+        Write-Verbose -Message "$($MyInvocation.MyCommand): Found version: $Version."
     }
+    else {
+        Write-Warning -Message "$($MyInvocation.MyCommand): Failed to determine version number from: $($Response.ResponseUri.AbsoluteUri)."
+    }
+
+    # Construct the output; Return the custom object to the pipeline
+    $PSObject = [PSCustomObject] @{
+        Version      = $Version
+        Date         = $Response.LastModified.DateTime
+        Architecture = Get-Architecture -String $Response.ResponseUri.AbsoluteUri
+        URI          = $Response.ResponseUri.AbsoluteUri
+    }
+    Write-Output -InputObject $PSObject
 }
