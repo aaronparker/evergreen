c02e4e14ffa0c6e290ef686ac75d1127c81e4f5f
Aaron Parker <aaron@stealthpuppy.com>
2025-03-21 10:02:41 +1100

    Update MicrosoftWvdBootLoader
    Update due to change in source URL

--- Evergreen/Apps/Get-MicrosoftWvdBootloader.ps1
+++ Evergreen/Apps/Get-MicrosoftWvdBootloader.ps1
@@ -17,31 +17,26 @@ function Get-MicrosoftWvdBootLoader {
         $res = (Get-FunctionResource -AppName ("$($MyInvocation.MyCommand)".Split("-"))[1])
     )
 
-    # Grab the download link headers to find the file name
     $params = @{
-        Uri          = $res.Get.Download.Uri
-        Method       = "Head"
-        ReturnObject = "Headers"
+        Uri = $res.Get.Download.Uri
     }
-    $Headers = Invoke-EvergreenWebRequest @params
-    if ($null -ne $Headers) {
+    $Response = Resolve-SystemNetWebRequest @params
 
-        # Match filename
-        $Filename = [RegEx]::Match($Headers['Content-Disposition'], $res.Get.Download.MatchFilename).Captures.Groups[1].Value
-
-        # Match version
-        $Version = [RegEx]::Match($Headers['Content-Disposition'], $res.Get.Download.MatchVersion).Captures.Value
-        if ($Version.Length -eq 0) { $Version = "Unknown" }
-
-        # Construct the output; Return the custom object to the pipeline
-        $PSObject = [PSCustomObject] @{
-            Version      = $Version
-            Architecture = Get-Architecture -String $Filename
-            Date         = $Headers['Last-Modified'] | Select-Object -First 1
-            Size         = $Headers['Content-Length'] | Select-Object -First 1
-            Filename     = $Filename
-            URI          = $res.Get.Download.Uri
-        }
-        Write-Output -InputObject $PSObject
+    # Match version
+    $Version = [RegEx]::Match($Response.ResponseUri.AbsoluteUri, $res.Get.Download.MatchVersion).Captures.Groups[1].Value
+    if ($null -ne $Version) {
+        Write-Verbose -Message "$($MyInvocation.MyCommand): Found version: $Version."
     }
+    else {
+        Write-Warning -Message "$($MyInvocation.MyCommand): Failed to determine version number from: $($Response.ResponseUri.AbsoluteUri)."
+    }
+
+    # Construct the output; Return the custom object to the pipeline
+    $PSObject = [PSCustomObject] @{
+        Version      = $Version
+        Date         = $Response.LastModified.DateTime
+        Architecture = Get-Architecture -String $Response.ResponseUri.AbsoluteUri
+        URI          = $Response.ResponseUri.AbsoluteUri
+    }
+    Write-Output -InputObject $PSObject
 }
