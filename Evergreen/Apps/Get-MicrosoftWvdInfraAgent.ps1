502072a6a7e13c963962e0425f60f13c8181f010
Aaron Parker <aaron@stealthpuppy.com>
2025-03-20 17:37:34 +1100

    Update MicrosoftWvdInfraAgent
    Update due to change in source URL

--- Evergreen/Apps/Get-MicrosoftWvdInfraAgent.ps1
+++ Evergreen/Apps/Get-MicrosoftWvdInfraAgent.ps1
@@ -18,25 +18,25 @@ function Get-MicrosoftWvdInfraAgent {
     )
 
     $params = @{
-        Uri          = $res.Get.Download.Uri
-        Method       = "Head"
-        ReturnObject = "Headers"
+        Uri = $res.Get.Download.Uri
     }
-    $Content = Invoke-EvergreenWebRequest @params
-    if ($null -ne $Content) {
+    $Response = Resolve-SystemNetWebRequest @params
 
-        # Match filename
-        $Filename = [RegEx]::Match($Content.'Content-Disposition', $res.Get.Download.MatchFilename).Captures.Groups[1].Value
-
-        # Construct the output; Return the custom object to the pipeline
-        $PSObject = [PSCustomObject] @{
-            Version      = [RegEx]::Match($Content.'Content-Disposition', $res.Get.Download.MatchVersion).Captures.Value
-            Date         = $Content.'Last-Modified'[0]
-            Architecture = Get-Architecture -String $Filename
-            Filename     = $Filename
-            URI          = $res.Get.Download.Uri
-        }
-        #if ($null -ne $Content.'Content-Length') { $PSObject | Add-Member -Name 'Size' -Type "NoteProperty" -Value $Content.'Content-Length'[0] }
-        Write-Output -InputObject $PSObject
+    # Match version
+    $Version = [RegEx]::Match($Response.ResponseUri.AbsoluteUri, $res.Get.Download.MatchVersion).Captures.Groups[1].Value
+    if ($null -ne $Version) {
+        Write-Verbose -Message "$($MyInvocation.MyCommand): Found version: $Version."
     }
+    else {
+        Write-Warning -Message "$($MyInvocation.MyCommand): Failed to determine version number from: $($Response.ResponseUri.AbsoluteUri)."
+    }
+
+    # Construct the output; Return the custom object to the pipeline
+    $PSObject = [PSCustomObject] @{
+        Version      = $Version
+        Date         = $Response.LastModified.DateTime
+        Architecture = Get-Architecture -String $Response.ResponseUri.AbsoluteUri
+        URI          = $Response.ResponseUri.AbsoluteUri
+    }
+    Write-Output -InputObject $PSObject
 }
