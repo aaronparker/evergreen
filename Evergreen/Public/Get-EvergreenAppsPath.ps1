baea9525f090df59f17015487f96e70be36994a9
Aaron Parker <aaron@stealthpuppy.com>
2025-07-07 13:42:26 +1000

    Add support for EVERGREEN_APPS_PATH environment variable
    The Get-EvergreenAppsPath function now checks for the EVERGREEN_APPS_PATH environment variable and returns its value if set, allowing users to override the default apps path location.

--- Evergreen/Public/Get-EvergreenAppsPath.ps1
+++ Evergreen/Public/Get-EvergreenAppsPath.ps1
@@ -1,4 +1,14 @@
 function Get-EvergreenAppsPath {
-    $AppsPath = if ($IsWindows) { Join-Path -Path ${Env:LOCALAPPDATA} -ChildPath 'Evergreen' } else { Join-Path -Path $HOME -ChildPath '.evergreen' }
-    return $AppsPath
+    if (Test-Path -Path Env:EVERGREEN_APPS_PATH) {
+        return ${Env:EVERGREEN_APPS_PATH}
+    }
+    else {
+        $AppsPath = if ($IsWindows) {
+            Join-Path -Path ${Env:LOCALAPPDATA} -ChildPath 'Evergreen'
+        }
+        else {
+            Join-Path -Path $HOME -ChildPath '.evergreen'
+        }
+        return $AppsPath
+    }
 }
