ee139dfe54f19ec55fdbbd506aceedefa6c83e4a
Aaron Parker <aaron@stealthpuppy.com>
2025-07-07 13:53:23 +1000

    Improve validation of EVERGREEN_APPS_PATH environment variable
    Adds a check to ensure the EVERGREEN_APPS_PATH environment variable points to a valid directory. If the path is invalid, a warning is issued and the original value is returned

--- Evergreen/Public/Get-EvergreenAppsPath.ps1
+++ Evergreen/Public/Get-EvergreenAppsPath.ps1
@@ -1,14 +1,15 @@
 function Get-EvergreenAppsPath {
     if (Test-Path -Path Env:EVERGREEN_APPS_PATH) {
-        return ${Env:EVERGREEN_APPS_PATH}
-    }
-    else {
-        $AppsPath = if ($IsWindows) {
-            Join-Path -Path ${Env:LOCALAPPDATA} -ChildPath 'Evergreen'
+        if (Test-Path -Path ${Env:EVERGREEN_APPS_PATH} -PathType "Container") {
+            return (Resolve-Path -Path ${Env:EVERGREEN_APPS_PATH}).Path
         }
         else {
-            Join-Path -Path $HOME -ChildPath '.evergreen'
+            Write-Warning -Message "Environment variable 'EVERGREEN_APPS_PATH' is set but does not point to a valid path."
+            return ${Env:EVERGREEN_APPS_PATH}
         }
+    }
+    else {
+        $AppsPath = if ($IsWindows) { Join-Path -Path ${Env:LOCALAPPDATA} -ChildPath 'Evergreen' } else { Join-Path -Path $HOME -ChildPath '.evergreen' }
         return $AppsPath
     }
 }
